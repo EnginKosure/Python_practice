f46e59b6448cc76a8d40ea904d27f0fcaebb6f6f
EnginKosure <engin.kosure@cronos.be>
2020-05-24 00:36:11 +0200

    basic calculating function added

--- day4.py
+++ day4.py
@@ -10,10 +10,10 @@ def convert_decimal_to_binary(num):
         i += 1
 
     # print(i)
-    binary = [0] * (i+1)
+    binary = [0] * (i + 1)
     while i > 0:
         x = int(num / (2 ** i))
-        binary[len(binary)-1 - i] = x
+        binary[len(binary) - 1 - i] = x
         num -= x * (2 ** i)
         i -= 1
     binary[-1] = num % 2
@@ -26,3 +26,33 @@ convert_decimal_to_binary(8)
 convert_decimal_to_binary(9)
 convert_decimal_to_binary(100)
 
+
+# Calculator -
+# A simple calculator to do basic operators. Make it a scientific calculator for added complexity.
+
+def calc(a,b,op):
+    if op=='+':
+        print('result of addition', a+b)
+    elif op=='-':
+        print('result of subtraction', a-b)
+    elif op=='*':
+        print('result of multiplication', a*b)
+    elif op=='/':
+        print('result of division', a/b)
+    else:
+        print('wrong input')
+
+
+def main():  # Wrapper function
+
+    a = int(input('Please type the first number: '))
+    b = int(input('Please type the second number: '))
+    op = input(
+        'What kind of operation would you like to do?\
+        \nChoose between "+, -, *, /" : ')
+
+    print(calc(a, b, op))
+
+
+if __name__ == '__main__':
+    main()
