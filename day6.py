17fa32f6c8234d510eaca7dcb9e263470faee5de
EnginKosure <engin.kosure@cronos.be>
2020-05-25 23:25:54 +0200

    collatz function added

--- day6.py
+++ day6.py
@@ -6,6 +6,23 @@ def exponent(a, b):
     print(a ** b)
 
 
+# Collatz Conjecture -
+# Start with a number n > 1. Find the number of steps it takes to reach one using the following process:
+# If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1.
+
+def collatz(n):
+    i = 0
+    while n != 1:
+        i += 1
+        if n % 2:
+            n = n * 3 + 1
+        else:
+            n /= 2
+    print(i)
+
+
+collatz(48)
+
 if __name__ == '__main__':
     input1 = int(input('enter base: '))
     input2 = int(input('enter exponent: '))
