db938dccdc72eddf4ee3b8249e84297bf3ea60b4
EnginKosure <engin.kosure@cronos.be>
2020-05-26 18:09:40 +0200

    leap year calculation added

--- day7.py
+++ day7.py
@@ -10,3 +10,20 @@ if __name__ == '__main__':
     print(
         f'{(seconds / seconds_in_a_day)} days or {(seconds / seconds_per_hour)} hours or \
 {(seconds / seconds_per_minute)} minutes')
+
+
+# leap year
+def is_leap_year(x):
+    if x % 400 == 0:
+        return True
+    elif x % 100 == 0:
+        return False
+    elif x % 4 == 0:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    input1 = int(input('Enter year to check if it is a leap year: '))
+    print(is_leap_year(input1))
