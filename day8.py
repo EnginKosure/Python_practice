8e2ae0cfa2128609a3ad8f08c17247caa8488371
EnginKosure <engin.kosure@cronos.be>
2020-05-27 07:58:33 +0200

    decription added

--- day8.py
+++ day8.py
@@ -1,7 +1,7 @@
 # Caesar cipher
 
 message = input('enter the message: ')
-shift = int(input('enter the key'))
+shift = int(input('enter the key: '))
 
 new_message = ''
 
@@ -19,3 +19,22 @@ for ch in message:
     else:
         new_message += ch
 print('The shifted message is', new_message)
+
+
+# print(ord('a'), ord('z'), ord('A'), ord('Z'))  # 97 122 65 90
+
+decrypted = ''
+for i in new_message:
+    if 'a' <= i <= 'z':
+        pos = ord(i) - ord('a')
+        pos = (pos - shift) % 26
+        new_i = chr(pos + ord('a'))
+        decrypted += new_i
+    elif 'A' <= i <= 'Z':
+        pos = ord(i) - ord('A')
+        pos = (pos - shift) % 26
+        new_i = chr(pos + ord('A'))
+        decrypted += new_i
+    else:
+        decrypted += i
+print('decrypted', decrypted)
