d0801e1008253793e5c4001f29835f993330d092
EnginKosure <engin.kosure@cronos.be>
2020-05-29 17:52:21 +0200

    password validation function added

--- day9.py
+++ day9.py
@@ -42,5 +42,31 @@ def some_wrapper_function():
     print('Random password is:', random_password())
 
 
+def password_control(p):
+    has_upper = False
+    has_lower = False
+    has_number = False
+    has_special = False
+
+    for ch in p:
+        if 'a' <= ch <= 'z':
+            has_lower = True
+        elif 'A' <= ch <= 'Z':
+            has_upper = True
+        elif '0' <= ch <= '9':
+            has_number = True
+        elif ord(ch) in [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92,
+                         93, 94, 95, 96, 123, 124, 125, 126]:
+            has_special = True
+    if len(p) >= 8 and has_special and has_upper and has_number and has_lower:
+        return True
+    return False
+
+
 if __name__ == '__main__':
     some_wrapper_function()
+    p = input('Enter a password to check if valid: ')
+    if password_control(p):
+        print('Valid password')
+    else:
+        print('Invalid password')
