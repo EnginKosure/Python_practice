e66f9ccd946c67376111bcf1ff00013ca09326c7
EnginKosure <engin.kosure@cronos.be>
2020-05-29 23:12:40 +0200

    list ex.

--- day10.py
+++ day10.py
@@ -1,1 +1,10 @@
 # Display the entered user data in ascending order
+data = []
+
+num = int(input('Enter an integer (0 to quit): '))
+
+while num != 0:
+    data.append(num)
+    num = int(input('Enter an integer (0 to quit): '))
+
+data.sort()
