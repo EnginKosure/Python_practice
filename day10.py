dba6654bd737bfc89d51956c2befa0389dcc954a
EnginKosure <engin.kosure@cronos.be>
2020-05-30 02:32:28 +0200

    hashmap solution for two sum algorithm

--- day10.py
+++ day10.py
@@ -14,4 +14,27 @@ print('The values in ascending order are: ')
 for num in data:
     print(num)
 
-# Remove outliers
+
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# hashMap usage
+
+def two_sum(l, target):
+    d = {}
+    # print(list(enumerate(l)))
+    for k, v in enumerate(l):
+        # print(k, v)
+        d[v] = k
+    # print(d)
+
+    c = []
+
+    for i in l:
+        if target - i in d.keys() and target - i != i:
+            c.append(d[i])
+    print(c)
+
+
+a = [2, 7, 11, 15]
+two_sum(a, 18)
+arr = [3, 2, 1, 1]
+two_sum(arr, 6)
