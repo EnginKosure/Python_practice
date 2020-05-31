789956a4b36017707af93115914312cfc06c6ea8
EnginKosure <engin.kosure@cronos.be>
2020-05-31 09:28:01 +0200

    shuffle card function added

--- day11.py
+++ day11.py
@@ -7,8 +7,7 @@ def create_deck():
     cards = []
 
     for suit in ['s', 'h', 'd', 'c']:
-        for value in ['2', '3', '4', '5', '6', '7', '8', '9',
-                      'T', 'J', 'Q', 'K', 'A']:
+        for value in ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']:
             cards.append(value + suit)
     return cards
 
@@ -17,9 +16,10 @@ def shuffle(cards):
     for i in range(0, len(cards)):
         ran_pos = randrange(0, len(cards))
 
-        temp = cards[i]
-        cards[i] = cards[ran_pos]
-        cards[ran_pos] = temp
+        # temp = cards[i]
+        # cards[i] = cards[ran_pos]
+        # cards[ran_pos] = temp
+        cards[i], cards[ran_pos] = cards[ran_pos], cards[i]
 
 
 def main():
