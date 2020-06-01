517a52c5173cfd6bce84e0bd746d748633b16be1
EnginKosure <engin.kosure@cronos.be>
2020-06-01 12:17:29 +0200

    tokenizing added

--- day12.py
+++ day12.py
@@ -1,13 +1,21 @@
 # tokenizing
 def token_list(s):
+    # remove all of the spaces
     s = s.replace(" ", "")
+    # loop through all of the characters in the string
+    # identify the tokens and add to list
     tokens = []
     i = 0
     while i < len(s):
+        # These characters below must always be handled as a single character
         if s[i] in ["*", "/", "^", "\\", "(", ")"]:
             tokens.append(s[i])
             i += 1
+
+        # Handle + and -
         elif s[i] in ["+", "-"]:
+            # if there is a previous character and it is a number or close bracket
+            # then the + or - is a token on its own
             if i > 0 and ('0' <= s[i - 1] <= '9' or s[i - 1] == ')'):
                 tokens.append(s[i])
                 i += 1
