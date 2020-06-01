d7001b5f1ee85eeb27826c6112f7f48c6d9e02fa
EnginKosure <engin.kosure@cronos.be>
2020-06-01 11:07:26 +0200

    tokenizing added

--- day12.py
+++ day12.py
@@ -1,25 +1,30 @@
 # tokenizing
-def tokenList(s):
+def token_list(s):
     s = s.replace(" ", "")
     tokens = []
     i = 0
     while i < len(s):
-        if s[i] in '*/^\()':
+        if s[i] in ["*", "/", "^", "\\", "(", ")"]:
             tokens.append(s[i])
             i += 1
-        elif s[i] in '+-':
-            if i > 0 and ('9' <= s[i - 1] >= '0' or s[i - 1] == ')'):
+        elif s[i] in ["+", "-"]:
+            if i > 0 and ('0' <= s[i - 1] <= '9' or s[i - 1] == ')'):
                 tokens.append(s[i])
                 i += 1
             else:
                 num = s[i]
                 i += 1
-        elif '0' <= s[i] >= '9':
+                while i < len(s) and '0' <= s[i] <= '9':
+                    num += s[i]
+                    i += 1
+                tokens.append(num)
+        elif '0' <= s[i] <= '9':
             num = ''
-            while i < len(s) and '0' <= s[i] >= '9':
+            while i < len(s) and '0' <= s[i] <= '9':
                 num += s[i]
                 i += 1
             tokens.append(num)
+
         else:
             return []
     return tokens
@@ -27,7 +32,7 @@ def tokenList(s):
 
 def main():
     exp = input('Enter a math expression: ')
-    tokens = tokenList(exp)
+    tokens = token_list(exp)
     print('The tokens are: ', tokens)
 
 
