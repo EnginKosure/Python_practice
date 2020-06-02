78ecb0d5d33784be7c15343c0a8a8077403c56fe
EnginKosure <engin.kosure@cronos.be>
2020-06-02 20:22:27 +0200

    reverse lookup func added

---
+++ day14.py
@@ -0,0 +1,22 @@
+# Reverse lookup
+
+
+def reverse_lookup(data, value):
+    keys = []
+
+    # check each key, adding it to keys if the values match
+    for key in data:
+        if data[key] == value:
+            keys.append(key)
+
+    return keys
+
+
+def main():
+    fr_en = {'le': 'the', 'la': 'the', 'livre': 'book', 'pomme': 'apple'}
+
+    print('The French words for "the" are: ', reverse_lookup(fr_en, 'the'))
+
+
+if __name__ == '__main__':
+    main()
