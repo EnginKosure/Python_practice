733bfdf4d7158bbb95ec8041b8baaa9607d6ae7a
EnginKosure <engin.kosure@cronos.be>
2020-06-03 07:11:41 +0200

    list comp. reviewed

--- day15.py
+++ day15.py
@@ -4,6 +4,15 @@ words = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']
 # for i in stuff:
 #     print(i)
 
-[print(i) for i in [[w.upper(), w.lower(), len(w)] for w in words]]
+# y = [print(i) for i in [[w.upper(), w.lower(), len(w)] for w in words]]
+# print(y) would return [None, None, None, None, None, None, None, None, None]
+
+x = [i for i in [[w.upper(), w.lower(), len(w)] for w in words]]
+
+
+print(x)  # print([i for i in x]) would give the same output
+# [['THE', 'the', 3], ['QUICK', 'quick', 5], ['BROWN', 'brown', 5], ['FOX', 'fox', 3],
+# ['JUMPS', 'jumps', 5], ['OVER', 'over', 4], ['THE', 'the', 3], ['LAZY', 'lazy', 4], ['DOG', 'dog', 3]]
+
 
 # Two dice simulation
