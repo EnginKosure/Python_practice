73a1fa7258044ef5b6151023293e3b996bbffc3b
EnginKosure <engin.kosure@cronos.be>
2020-06-05 00:16:06 +0200

    anagram check added

--- day16.py
+++ day16.py
@@ -1,16 +1,34 @@
 # unique chars
+# anagram check
 
-s = input('Enter chars: ')
 
-chars = {}
-for i in s:
-    #keys = chars.keys()
-    # if i in keys:
-    if i in chars:
-        chars[i] += 1
+def char_counts(s):
+    chars = {}
+    for i in s:
+        # keys = chars.keys()
+        # if i in keys:
+        if i in chars:
+            chars[i] += 1
+        else:
+            chars[i] = 1
+    print('this string contains', len(chars), 'unique chars.')
+    print(s)
+    print(''.join(chars.keys()))
+    return chars
+
+
+def main():
+    s1 = input('Enter chars: ')
+    s2 = input('Enter chars: ')
+
+    seq1 = char_counts(s1)
+    seq2 = char_counts(s2)
+
+    if seq1 == seq2:
+        print('Those strings are anagrams')
     else:
-        chars[i] = 1
-print(chars)
-print('this string contains', len(chars), 'unique chars.')
-print(s)
-print(''.join(chars.keys()))
+        print('Not anagram')
+
+
+if __name__ == '__main__':
+    main()
