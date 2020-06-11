67b949654adf05cbf22a4026f70aebe61562368f
EnginKosure <engin.kosure@cronos.be>
2020-06-11 22:02:08 +0200

    basic plot functionality tested

---
+++ trial.py
@@ -0,0 +1,13 @@
+print('www', 'clarusway', "com", sep='.', end=' ')
+print('will', end=' ')
+print('open', end=' ')
+print('your', end=' ')
+print('path', end='.')
+# www.clarusway.com will open your path.
+
+a = 36.5
+b = '30'
+c = '3.5'
+d = ' F is enough for room temperature.'
+
+print(str(a+int(b)+float(c))+d)
