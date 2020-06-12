4a68029448f9eead573199f130c6b8f8b382a811
EnginKosure <engin.kosure@cronos.be>
2020-06-12 23:28:51 +0200

    lambda version added

--- day22.py
+++ day22.py
@@ -1,10 +1,20 @@
 import math
 
+'''
+from math import degrees
+def to_degree(radians):
+    return round(degrees(radians))
+'''
+
 
 def to_degree(radian):
     return round(180*radian/math.pi)
 
-# to_degree = lambda r: r/math.pi*180
+
+#to_degree2 = lambda r: round(r/math.pi*180)
+# to_degree2 = lambda r: r/math.pi*180
+def to_degree2(r): return round(r/math.pi*180)
 
 
 print(to_degree(6.18))
+print(to_degree2(3.142*2))
