34d4b898aa932585c27e35b7faae64a24d785fcd
EnginKosure <engin.kosure@cronos.be>
2020-06-12 20:48:51 +0200

    sending command line parameters tested

--- day20.py
+++ day20.py
@@ -5,4 +5,32 @@ num_lines = 10
 
 if len(sys.argv) != 2:
     print('Please provide the file name as a command line parameter.')
-    quit()
+    quit()  # When quit func. called, the program ends immediately.
+
+try:
+    # Open the file for reading.
+    inf = open(sys.argv[1], 'r')
+    # read the first line from the file
+    line = inf.readline()
+
+    # Keep looping until 10 or eof
+    count = 0
+    while count < num_lines and line != '':
+        # Remove the trailing newline char and count the line
+        line = line.rstrip()
+        count += 1
+
+        # display the line
+        print(line)
+
+        # read the next line
+        line = inf.readline()
+    # close the file
+    inf.close()
+except IOError:
+    # Display a message if sth goes wrong while accessing the file
+    print("IOError-could not access the file")
+
+# From terminal, execute like below
+# python day20.py trial.py
+# this code will read inside of the file provided as a second argument.
--- trial.py
+++ trial.py
@@ -2,12 +2,11 @@ print('www', 'clarusway', "com", sep='.', end=' ')
 print('will', end=' ')
 print('open', end=' ')
 print('your', end=' ')
-print('path', end='.')
+print('path', end='.\n')
 # www.clarusway.com will open your path.
-
 a = 36.5
 b = '30'
 c = '3.5'
 d = ' F is enough for room temperature.'
-
 print(str(a+int(b)+float(c))+d)
+# 70.0 F is enough for room temperature.
