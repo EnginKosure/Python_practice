090a3003b4089d7f30cff55b57eb45bf3488655b
EnginKosure <engin.kosure@cronos.be>
2020-06-13 01:32:09 +0200

    pytest explored

--- test_compute_launch.py
+++ test_compute_launch.py
@@ -15,3 +15,6 @@ def test_days_until_launch_0_negative():
 
 def test_days_until_launch_1():
     assert(days_until_launch(9, 10) == 1)
+
+# The name of the test file must start with test_, conventionally the same with the test_file_to_be_tested
+# only writing pytest on terminal is enough for running tests.
