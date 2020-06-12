950a6994fcfa82e3dc075b8826b3d861e06767d6
EnginKosure <engin.kosure@cronos.be>
2020-06-12 23:47:46 +0200

    date obj. explored

--- day22.py
+++ day22.py
@@ -1,7 +1,9 @@
 import math
+import datetime
 
 '''
 from math import degrees
+#degrees() method ->Converts angles from radians to degrees.
 def to_degree(radians):
     return round(degrees(radians))
 '''
@@ -18,3 +20,11 @@ def to_degree2(r): return round(r/math.pi*180)
 
 print(to_degree(6.18))
 print(to_degree2(3.142*2))
+
+
+def time_for_milk_and_cookies(date):
+    return date.month == 12 and date.day == 24
+    # return  [date.month,date.day] == [11,24]
+
+
+print(time_for_milk_and_cookies(datetime.date(2013, 12, 24)))  # True
