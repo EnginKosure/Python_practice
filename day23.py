d4ba51f4a7ae1f1ac7ea8f02164e7af74f82b7dd
EnginKosure <engin.kosure@cronos.be>
2020-06-14 22:54:44 +0200

    file operations in py

--- day23.py
+++ day23.py
@@ -4,3 +4,16 @@ import sys
 if len(sys.argv) == 1:
     print("Provide at least one file name")
     quit()
+for i in range(1, len(sys.argv)):
+    f_name = sys.argv[i]
+    try:
+        inf = open(f_name, 'r')
+
+        # Display the file
+        for line in inf:
+            print(line, end='')
+        # close the file
+        inf.close()
+    except:
+        # Display a message but don't quit.
+        print('Could not open', f_name)
