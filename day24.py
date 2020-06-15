ad48b7dde063de720ceab7051f045841b650f013
EnginKosure <engin.kosure@cronos.be>
2020-06-15 23:46:28 +0200

    file operations, removing comments.

---
+++ day24.py
@@ -0,0 +1,19 @@
+# Remove comments from a py file
+
+# Read and open the input file. Ensure it is opened
+
+# Read and open output file. Ensure it is successfull.
+
+
+# Read all lines from input file, remove comments.
+# Save the lines to a new file.
+
+# Find the position of the comment character.
+# -1 if there is not any.
+
+# If there is a comment, then form a slice of the string that excludes it.
+# Overwrite the line.
+
+# Write the modified line to the file
+
+# Close the files
