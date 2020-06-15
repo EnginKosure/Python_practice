6ec4f38f36508a99446ce733daf10b13657a55ba
EnginKosure <engin.kosure@cronos.be>
2020-06-15 23:58:04 +0200

    file operations, removing comments.

--- day24.py
+++ day24.py
@@ -4,23 +4,33 @@
 
 try:
     input_file_name = input("Enter the name of the python file: ")
-    inf = open(input_file_name, 'r')
+    in_f = open(input_file_name, 'r')
 except:
     print('Could not open the input file.')
     quit()
 
 # Read and open output file. Ensure it is successfull.
-
+try:
+    output_file_name = input("Enter the name of the output file: ")
+    out_f = open(output_file_name, 'w')
+except:
+    print('Error opening the output file.')
+    quit()
 
 # Read all lines from input file, remove comments.
 # Save the lines to a new file.
+try:
+    for line in in_f:
 
-# Find the position of the comment character.
-# -1 if there is not any.
+        # Find the position of the comment character.
+        # -1 if there is not any.
+        pos = line.find('#')
 
-# If there is a comment, then form a slice of the string that excludes it.
-# Overwrite the line.
-
-# Write the modified line to the file
-
-# Close the files
+        # If there is a comment, then form a slice of the string that excludes it.
+        # Overwrite the line.
+        if pos > -1:
+            line = line[0:pos]
+            line = line+'\n'
+        # Write the modified line to the file
+        out_f.write(line)
+        # Close the files
