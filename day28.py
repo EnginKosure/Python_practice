95b24d15383541494df1973049b3cacfb482c16c
EnginKosure <engin.kosure@cronos.be>
2020-06-18 16:31:44 +0200

    string challenge solved

--- day28.py
+++ day28.py
@@ -1,15 +1,31 @@
-control_string = 'aeiou'
+# control_string = 'aeiou'
 
 
-def find_vocals(s):
+# def find_vocals(s):
 
-    result = ''
+#     result = ''
 
-    for character in s:
-        if character in control_string:
-            result += character
-    # to avoid the repeatings
-    return ''.join(set(result))
+#     for character in s:
+#         if character in control_string:
+#             result += character
+#     # to avoid the repeatings
+#     return ''.join(set(result))
 
 
-print(find_vocals('Clarusway'))  # au
+# print(find_vocals('Clarusway'))  # au
+
+def vowels(s):
+    vowel = 'aeiou'
+    finded = ''
+    for i in s:
+        if i in vowel:
+            if not i in finded:
+                finded += i
+            # else:
+            #     continue
+        # else:
+        #     continue
+    return finded
+
+
+print(vowels('Merhaba arkadaslarim'))
