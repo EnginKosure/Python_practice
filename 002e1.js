0bc870e05a5e74e532c7ac0527a1838b4870aabd
EnginKosure <engin.kosure@cronos.be>
2020-06-22 16:59:26 +0200

    js challenge solved

--- 002e1.js
+++ 002e1.js
@@ -7,21 +7,18 @@
 
 
 function formatPercentage(arr) {
-    console.log(arr.toString().split(','));
-
     let newFormatted = [];
-    // let formatStringNumbers = parseFloat(arr.toString().split(",")).toFixed(2);
-    // console.log(formatStringNumbers);
+    let formatStringNumbers = arr.toString().split(",").toFixed(2);
 
-    // for (let i = 0; i < formatStringNumbers.length; i++) {
-    //     if (formatStringNumbers[i] > 100) {
-    //         formatStringNumbers[i] = formatStringNumbers[100];
-    //     } else {
-    //         newFormatted = formatStringNumber.push(newFormatted);
-    //         return newFormatted;
-    //         console.log(newFormatted);
-    //     }
-    // }
+    for (let i = 0; i < formatStringNumbers.length; i++) {
+        if (formatStringNumbers[i] > 100) {
+            formatStringNumbers[i] = formatStringNumbers[100];
+        } else {
+            newFormatted = formatStringNumber.push(newFormatted);
+            return newFormatted;
+            console.log(newFormatted);
+        }
+    }
     console.log(newFormatted);
 }
 console.log(formatPercentage([2, 50, 102, 30]));
