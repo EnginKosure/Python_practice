562d936ab09e4f4be1092ca3225018482b2679b1
EnginKosure <engin.kosure@cronos.be>
2020-06-23 19:06:48 +0200

    sort algorithm implemented

--- 002e2.js
+++ 002e2.js
@@ -1,13 +1,35 @@
+// function sortAges(arr) {
+//     let maximum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (typeof arr[i] !== "number") {
+//             //removes one element from the index i and 
+//             // to check the next number, decreases the index --
+//             arr.splice(i, 1); i--;
+//         }
+//         for (let i = 0; i < arr.length; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 let tmp = arr[i];
+//                 arr[i] = arr[i + 1];
+//                 arr[i + 1] = tmp;
+//             }
+//         }
+//     }
+//     console.log(arr);
+//     return arr;
+// }
+
 function sortAges(arr) {
-    let maximum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] !== "number") {
-            //removes one element from the index i and 
-            // to check the next number, decreases the index --
-            arr.splice(i, 1); i--;
-        }
+    let done = false;
+    while (!done) {
+        done = true;
         for (let i = 0; i < arr.length; i++) {
+            if (typeof arr[i] !== "number") {
+                //removes one element from the index i and 
+                // to check the next number, decreases the index --
+                arr.splice(i, 1); i--;
+            }
             if (arr[i] > arr[i + 1]) {
+                done = false;
                 let tmp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = tmp;
