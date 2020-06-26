ee5a9657c1434be82651bd485802d830efae402e
EnginKosure <engin.kosure@cronos.be>
2020-06-26 11:09:02 +0200

    improved

---
+++ deneme1.py
@@ -0,0 +1,16 @@
+arr = [1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 10]
+arr[3:3] = 'string'
+print(arr)
+
+s = ['aaaa', 'bbbb', 'cccc', 'dddd']
+s[2:2] = 'pppp'
+# print(s)
+# arr[2:2] = 55
+# [1, 2, 2, 's', 't', 'r', 'i', 'n', 'g', 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 10]
+print(arr[3])  # 's'
+print(arr[3:4])  # ['s']
+# What is written to put 'George' between 'Bill' and 'Ronald'?
+presidents = ['Barrack', 'George', 'Bill',
+              'Ronald', 'Jimmy', 'Gerald', 'Richard']
+presidents[3:3] = ['George']
+print(presidents)
