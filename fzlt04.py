779d65b9059d58254c21cce249d8cef3a25973ea
EnginKosure <engin.kosure@cronos.be>
2020-06-29 11:51:41 +0200

    2d array challenge added

---
+++ fzlt04.py
@@ -0,0 +1,19 @@
+def can_see_stage(seats):
+    count = 0
+    for j in range(len(seats[0])):
+        A = []
+        for i in range(len(seats)):
+            A.append(seats[i][j])
+        print(A)
+        if A == sorted(A) and len(A) == len(set(A)):
+            count += 1
+    return count == len(seats[0])
+
+
+a = [[1, 2, 3, 2, 1, 1],
+     [2, 4, 4, 3, 2, 2],
+     [5, 5, 5, 5, 4, 4],
+     [6, 6, 7, 6, 5, 5]]
+
+can_see_stage(a)
+print(can_see_stage(a))
