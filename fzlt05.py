fd6654fd5b52edb68dd24e255f1bdf627232669c
EnginKosure <engin.kosure@cronos.be>
2020-07-01 10:31:19 +0200

    advanced sort implemented

--- fzlt05.py
+++ fzlt05.py
@@ -5,5 +5,8 @@ def advanced_sort(lst):
 print(advanced_sort([7, 'a', 7, 'b', 10, 'a', 'b', 'c', '10']))
 
 print(dict.fromkeys([7, 'a', 7, 'b', 10, 'a', 'b', 'c', '10']))
+# {7: None, 'a': None, 'b': None, 10: None, 'c': None, '10': None}
 
-print(['a']*2)
+
+# print(['a']*3)  # ['a', 'a', 'a]
+# print([1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, ].count(3))  # 6
