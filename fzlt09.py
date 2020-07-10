487f6ac7c15b09eaa1d667f206156a04e527e2c7
EnginKosure <engin.kosure@cronos.be>
2020-07-10 11:42:37 +0200

    print implemented

--- fzlt09.py
+++ fzlt09.py
@@ -1,3 +1,28 @@
+from time import time
 for i in range(10):
     print(' '*(10-i)+str(i)*(2*i+1))
     print((2*i+1))
+
+
+def is_prime(num):
+    # num = int(input("enter a number : "))
+
+    if num <= 1:
+        print(f"{num} is NOT a prime number")
+
+    elif num % 2 == 0 and num != 2:
+        print(f"{num} is NOT a prime number")
+
+    else:
+        for i in range(3, int(num**0.5)+1, 2):
+            if num % i == 0:
+                print(f"{num} is NOT a prime number")
+                break
+        else:
+            print(f"{num} is a prime number")
+
+
+start = time()
+for i in range(1000):
+    is_prime(i)
+print(f'TIME{time()-start}')
