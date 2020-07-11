946bc554a0a2a9885e46ca02965527679c6c5860
EnginKosure <engin.kosure@cronos.be>
2020-07-11 03:19:15 +0200

    sum check solution added

--- fzlt10.py
+++ fzlt10.py
@@ -2,3 +2,27 @@
 
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 # return [0, 1]
+
+
+def two_sum(nums, target):
+    d = {}
+    for k, v in enumerate(nums):
+        # 0, 2
+        # 1,7
+        # 2,11
+        # 3,15
+        r = target-v
+        # 7=18-11
+        if r in d:
+            print([d[r], k])
+            return [d[r], k]
+        print(k)
+        # print(d[v])
+        d[v] = k
+        # d[2]=0
+        # d[7]=1
+        print(d)
+    return []
+
+
+two_sum([2, 7, 11, 15], 18)
