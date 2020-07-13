74b04d7a80d9decf1dfa3d77b84a261544d80a6f
EnginKosure <engin.kosure@cronos.be>
2020-07-14 00:48:07 +0200

    recursion revisited

--- fzlt13.py
+++ fzlt13.py
@@ -6,3 +6,13 @@ def reverse(string):
 
 
 print(reverse("hello"))  # olleh
+
+
+def sum(list):
+    if len(list) == 1:
+        return list[0]
+    else:
+        return list[0] + sum(list[1:])
+
+
+print(sum([5, 7, 3, 8, 10]))
