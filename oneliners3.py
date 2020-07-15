b6acd7a8b2560a2328afbc00f25378a4d9bbf2dd
EnginKosure <engin.kosure@cronos.be>
2020-07-16 00:28:25 +0200

    Daily oneliner examined.

--- oneliners3.py
+++ oneliners3.py
@@ -1,6 +1,6 @@
 # Data
 t = ['lambda functions are anonymous functions.',
-     'anonymous functions dont have a name.',
+     'anonymous functions do not have a name.',
      'functions are objects in Python.']
 q = 'anonymous'  # query
 
@@ -9,3 +9,6 @@ mark = map(lambda s: (1, s) if q in s else (0, s), t)
 
 # Result
 print(list(mark))
+# [(1, 'lambda functions are anonymous functions.'),
+# (1, 'anonymous functions do not have a name.'),
+# (0, 'functions are objects in Python.')]
