76833475ea0c0e84ba8d5877b9257973b39a3a17
EnginKosure <engin.kosure@cronos.be>
2020-07-15 23:54:18 +0200

    daily oneliner added

--- oneliners2.py
+++ oneliners2.py
@@ -9,13 +9,3 @@ for line in f:
     lines.append(line.strip())
 
 print(lines)
-'''
-['filename = "readFileDefault.py" # this code',
-'', '',
-'f = open(filename)',
-'lines = []',
-'for line in f:',
-'lines.append(line.strip())',
-'', '',
-'print(lines)']
-'''
