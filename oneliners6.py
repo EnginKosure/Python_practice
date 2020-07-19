16385ac7b4d83caa70d36e8c9518181e412a4212
EnginKosure <engin.kosure@cronos.be>
2020-07-19 23:19:10 +0200

    daily challenge solved

--- oneliners6.py
+++ oneliners6.py
@@ -13,3 +13,24 @@ print(sample)
 #  [9.5, 9.4, 9.2],
 #  [8.4, 7.9, 8.0],
 #  [7.1, 4.8, 4.7]]
+
+
+# Data
+visitors = ['Firefox', 'corrupted',
+            'Chrome', 'corrupted',
+            'Safari', 'corrupted',
+            'Safari', 'corrupted',
+            'Chrome', 'corrupted',
+            'Firefox', 'corrupted']
+
+# One-Liner
+visitors[1::2] = visitors[::2]
+
+# Result
+print(visitors)
+# ['Firefox', 'Firefox',
+#  'Chrome', 'Chrome',
+#  'Safari', 'Safari',
+#  'Safari', 'Safari',
+#  'Chrome', 'Chrome',
+#  'Firefox', 'Firefox']
