8d82e41f1ef07eb8e278702f8d2bf8f1acf6eb62
EnginKosure <engin.kosure@cronos.be>
2020-07-25 04:24:47 +0200

    missing char solved

--- fzlt23.py
+++ fzlt23.py
@@ -1,11 +1,30 @@
-def missing_char(word, n):
-    new_word = ''
-    for i in range(len(word)):
-        if i != n:
-            new_word += word[i]
-    print(new_word)
-    return new_word
+# def missing_char(word, n):
+#     new_word = ''
+#     for i in range(len(word)):
+#         if i != n:
+#             new_word += word[i]
+#     print(new_word)
+#     return new_word
 
 
-missing_char('fazilet', 1)
-missing_char('serra', 2)
+# missing_char('fazilet', 1)
+# missing_char('serra', 2)
+def make_box(n):
+    lst = []
+    for i in range(n):
+        if i == 0 or i == n-1:
+            lst.append('#'*n)
+        else:
+            lst.append('#'+' '*(n-2)+'#')
+    print(lst)
+    return lst
+
+
+make_box(5)
+
+
+def box(n):
+    x = ['\"'+n*"#"+'\",' if x == 0 or x == n -
+         1 else '\"#'+(n-2)*" "+'#\",' for x in range(n)]
+    for i in range(n):
+        print(x[i])
