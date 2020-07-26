030fe8d83dddf5bc45ff82e5d15c22ef5b87d3e1
EnginKosure <engin.kosure@cronos.be>
2020-07-26 23:50:52 +0200

    google interview questions implemented

---
+++ fzlt30.py
@@ -0,0 +1,30 @@
+# def permute(xs, low=0):
+#     if low + 1 >= len(xs):
+#         yield xs
+#     else:
+#         for p in permute(xs, low + 1):
+#             yield p
+#         for i in range(low + 1, len(xs)):
+#             xs[low], xs[i] = xs[i], xs[low]
+#             for p in permute(xs, low + 1):
+#                 yield p
+#             xs[low], xs[i] = xs[i], xs[low]
+
+
+# for p in permute([1, 2, 3, 4]):
+#     print(p)
+
+def permute(numbers):
+    result = [[]]
+    for n in numbers:
+        my_perms = []
+        for p in result:
+            for i in range(len(p)+1):
+                my_perms.append(p[:i] + [n] + p[i:])
+                result = my_perms
+    return result
+
+
+xx = [1, 2, 3]
+
+print(permute(xx))
