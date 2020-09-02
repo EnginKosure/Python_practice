f680b7c3b966a6f1e7e6de40f5c7fff78d4c0d14
EnginKosure <engin.kosure@cronos.be>
2020-09-02 23:16:56 +0200

    daily challenge finished

--- fzlt33.py
+++ fzlt33.py
@@ -4,3 +4,14 @@
 # The array can contain duplicates and negative numbers as well.
 # For example, the input[3, 4, -1, 1] should give 2. The input[1, 2, 0] should give 3.
 # You can modify the input array in-place.
+
+
+def find_missing(array):
+    array = sorted(array)
+    array_pos = [i for i in array if i >= 0]
+    control = list(range(array_pos[0], array_pos[-1]+1))
+    return array_pos[-1] + 1 if control == array_pos else list(set(control).difference(array_pos))[0]
+
+
+print(find_missing([1, 2, 0]))
+print(find_missing([3, 4, -1, 1]))
