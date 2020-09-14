923c2921edf057b671dad9337ddfc7cfd92367c5
EnginKosure <engin.kosure@cronos.be>
2020-09-14 23:11:28 +0200

    daily challenge solved

--- fzlt36.py
+++ fzlt36.py
@@ -2,3 +2,12 @@
 # What if, instead of being able to climb 1 or 2 steps at a time,
 #  you could climb any number from a set of positive integers X?
 # For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+
+
+def staircase(n, X):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        return sum(staircase(n - x, X) for x in X)
