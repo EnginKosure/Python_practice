86504304e765f42f2e3cd5c52e6f58a1cbc7237e
EnginKosure <engin.kosure@cronos.be>
2020-09-14 23:10:58 +0200

    daily challenge started

---
+++ fzlt36.py
@@ -0,0 +1,4 @@
+# daily-python-challenge
+# What if, instead of being able to climb 1 or 2 steps at a time,
+#  you could climb any number from a set of positive integers X?
+# For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
