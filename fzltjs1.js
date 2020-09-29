7d00075ad3bf6c98f73640bc18dc4df1e08c6652
EnginKosure <engin.kosure@cronos.be>
2020-09-29 19:02:16 +0200

    js recap

--- fzltjs1.js
+++ fzltjs1.js
@@ -1,13 +1,13 @@
 /* 1) Çarpma işareti olmadan çarpma işlemi yapan bir fonksiyon yazın: 
     * sumWithoutMultiply(3, 5)
 Output: 15 
-2) Verilen parayı banknot olarak hesaplayan, terminale ona göre çıktı yazan dongüsel bir fonksiyon yazın.
+2) Verilen parayı note olarak hesaplayan, terminale ona göre çıktı yazan dongüsel bir fonksiyon yazın.
     * calculateMoney(501)
 Output: 
     Current money is 501: 2 x 200
     Current money is 101: 1 x 100
     Current money is 1: 1 x 1
- (Banknot için array: [200, 100, 50, 20, 10, 5, 1])*/
+ (note için array: [200, 100, 50, 20, 10, 5, 1])*/
 
  function sumWithoutMultiply(x,y){
      let sum=0;
@@ -18,3 +18,17 @@ Output:
  }
 
  console.log(sumWithoutMultiply(3,5))
+
+ function calculateMoney(money) {
+    let result = ''
+    const note = [200, 100, 50, 20, 10, 5, 1]
+    for (let i = 0; i < note.length;i++){
+        if(money >= note[i]){
+            let divFirst = Math.floor(money/note[i])
+            result += `Current money is ${money}: ${divFirst} x ${note[i]}\n`
+            money = money % note[i];
+        }
+    }
+return(result)
+}
+console.log(calculateMoney(7121))
