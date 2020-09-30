b7c5c5bc4f655ea31a42460c15ad375ce21a5a97
EnginKosure <engin.kosure@cronos.be>
2020-09-30 22:02:46 +0200

    refactor algorithm

--- fzlt44.py
+++ fzlt44.py
@@ -24,3 +24,7 @@ def check_bishops(bishops, m):
             result += abs(bishops[i+1+j][0] - bishops[i][0]
                           ) == abs(bishops[i+1+j][1] - bishops[i][1])
     return result
+
+
+def check_bishops2(bishops, m):
+    return len([1 for i in range(len(bishops)) for j in range(len(bishops[i+1:])) if abs(bishops[i+1+j][0] - bishops[i][0]) == abs(bishops[i+1+j][1] - bishops[i][1])])
