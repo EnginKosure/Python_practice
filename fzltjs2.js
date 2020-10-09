01cb8b29010ac40c14aebe1c5ec3fa7e927cbf62
EnginKosure <engin.kosure@cronos.be>
2020-10-09 22:39:22 +0200

    add comments

--- fzltjs2.js
+++ fzltjs2.js
@@ -1,3 +1,4 @@
+// Unlike for...of, you get the letter index instead of the letter. It performs pretty badly.
 function reverseIt(s) {
   let rev = "";
   for (i in s) {
@@ -8,6 +9,7 @@ function reverseIt(s) {
 }
 reverseIt("fazilet"); //telizaf
 
+/*for...of is the new ES6 for iterator. Supported by most modern browsers. It is visually more appealing and is less prone to typing mistakes. If you are going for this one in a production application, you should be probably using a transpiler like Babel.*/
 function reverseIt2(s) {
   let rev = "";
   for (i of s) {
