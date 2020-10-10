70171bb89db2fd7d875d4a4dce41d8310e79b2b7
EnginKosure <engin.kosure@cronos.be>
2020-10-10 18:17:45 +0200

    daily python challenge started

---
+++ fzlt46.py
@@ -0,0 +1,23 @@
+waarde = int(input("Geef een getal: "))
+volume = 0
+control_variable = waarde
+
+while waarde != 0:
+    if waarde > 0:
+        control_variable = waarde
+        volume += waarde**2
+        waarde = int(input("Geef een getal: "))
+        # Burada if kontrolu buyuk sayiyi yakalayamiyordu, control_variable diye bir degisken olusturup
+        # bir onceki degeri uzerine yazarak sakladik.
+        if waarde >= control_variable:
+            volume = 0
+
+        else:
+            volume + (waarde**2)
+            print(volume)
+    else:
+        while waarde < 0:
+            # Negatif sayi gelirse volume u sifirlamamiz gerekiyor
+            volume = 0
+            waarde = int(input("Geef een getal: "))
+print("De volume van de piramide is", volume)
