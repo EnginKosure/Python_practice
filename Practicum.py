8650795cba0912f4fc24e8f8e19a271eba7c3fc4
EnginKosure <engin.kosure@cronos.be>
2020-10-11 23:28:21 +0200

    add test file

--- Practicum.py
+++ Practicum.py
@@ -7,8 +7,6 @@ while waarde != 0:
         control_variable = waarde
         volume += waarde**2
         waarde = int(input())
-        # Burada if kontrolu buyuk sayiyi yakalayamiyordu, control_variable diye bir degisken olusturup
-        # bir onceki degeri uzerine yazarak sakladik. Bu haliyle tum testleri geciyor.
         if waarde >= control_variable:
             volume = 0
 
@@ -16,10 +14,9 @@ while waarde != 0:
             volume + (waarde**2)
     else:
         while waarde < 0:
-            # Negatif sayi gelirse volume u sifirlamamiz gerekiyor
+
             volume = 0
             waarde = int(input())
-            # BURADA GUNCELLEDIK!!!
             control_variable = waarde
 
 print(volume)
