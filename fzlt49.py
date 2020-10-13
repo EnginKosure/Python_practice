cbcd8dc2e25218d749ca39f458ca01cd92040e19
EnginKosure <engin.kosure@cronos.be>
2020-10-13 23:27:43 +0200

    implement recursive one-liner

--- fzlt49.py
+++ fzlt49.py
@@ -23,5 +23,25 @@ def div_wod1(x, y, res=0):
         return res
 
 
-print(div_wod1(11, 20))  # 0
-print(div_wod1(15, 3))  # 5
+def div_wod2(x, y, res=0):
+    if x < y:
+        return res
+    else:
+        res += 1
+        return div_wod1(x-y, y, res)
+
+
+print(div_wod2(11, 20))  # 0
+print(div_wod2(20, 4))  # 5
+
+
+def d(x, y, res=0, c=1): return res if x < y else d(x-y, y, res+c, c)
+
+
+print(d(20, 20))  # 1
+print(d(24, 4))  # 6
+
+# q = lambda l: q([x for x in l[1:] if x <= l[0]]) + [l[0]] + q([x for x in l if x > l[0]]) if l else []
+
+# ## The Result
+# print(q(unsorted))
