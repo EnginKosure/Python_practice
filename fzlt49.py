cdceb14d238148fda7720b4003ac97becbc81c69
EnginKosure <engin.kosure@cronos.be>
2020-10-13 22:22:19 +0200

    implement first solution

--- fzlt49.py
+++ fzlt49.py
@@ -1,3 +1,11 @@
 # Implement division of two positive integers without using
 # the division, multiplication, or modulus operators.
 # Return the quotient as an integer, ignoring the remainder.
+
+
+def div_wod(x, y):
+    res = 0
+    while y <= x:
+        x -= y
+        res += 1
+    return res
