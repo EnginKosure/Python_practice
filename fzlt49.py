68a8e496595fc9d52d9364724873152955915191
EnginKosure <engin.kosure@cronos.be>
2020-10-13 22:48:00 +0200

    implement a recursional approach

--- fzlt49.py
+++ fzlt49.py
@@ -11,5 +11,17 @@ def div_wod(x, y):
     return res
 
 
-print(div_wod(11, 20))  # 0
-print(div_wod(13, 3))  # 4
+# print(div_wod(11, 20))  # 0
+# print(div_wod(13, 3))  # 4
+
+
+def div_wod1(x, y, res=0):
+    if x > y:
+        res += 1
+        return div_wod1(x-y, y, res)
+    else:
+        return res
+
+
+print(div_wod1(11, 20))  # 0
+print(div_wod1(15, 3))  # 4
