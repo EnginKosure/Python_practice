fc8224a838f3bdd5e04c2699498cc6f93cf8bed4
EnginKosure <engin.kosure@cronos.be>
2020-10-14 23:22:56 +0200

    implement solution with import

--- fzlt50.py
+++ fzlt50.py
@@ -1,3 +1,8 @@
 # Given an integer n and a list of integers,
 # write a function that randomly generates a number
 # from 0 to n-1 that isn't in that given integer list.
+from random import choice
+
+
+def random_int(n, l):
+    return choice([i for i in range(n) if i not in l])
