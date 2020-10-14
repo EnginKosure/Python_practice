6db59b005958f2024e0917d4ec35dc38ee6f4388
EnginKosure <engin.kosure@cronos.be>
2020-10-14 23:42:45 +0200

    implement solution without import

--- fzlt50.py
+++ fzlt50.py
@@ -6,3 +6,12 @@ from random import choice
 
 def random_int(n, l):
     return choice([i for i in range(n) if i not in l])
+
+
+def random_int1(seed, l):
+    def rand():
+        rand.seed = (1103515245*rand.seed + 12345) & 0x7fffffff
+        return rand.seed
+    rand.seed = seed
+    if rand not in l:
+        return rand
