a3bcb39a7449ff421433c4e5e1db96c27795b65c
EnginKosure <engin.kosure@cronos.be>
2020-10-15 13:56:16 +0200

    js recap

--- fzlt51.js
+++ fzlt51.js
@@ -3,7 +3,7 @@ myArray = ["clarusway", "google", "adobe", "cisco", 'space - x', "clarusway", "c
 // Output:
 // { clarusway: 4, google: 2, adobe: 1, cisco: 2, spaceX: 1, facebook: 1 }
 
-const c = a => a.reduce((p, c) => (p[c] = ++p[c] || 1, p), {});
+const c = a => a.reduce((a, c) => (a[c] = ++a[c] || 1, a), {});
 console.log(c(myArray))
 
 let names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice']
@@ -23,26 +23,5 @@ let countedNames = names.reduce(function (allNames, name) {
 let countedNames1 = (x) => x.reduce((acc, curr) => (acc[curr] = ++acc[curr] || 1, acc), {})
 
 console.log(countedNames1(names))
-//1-->acc {}                   ## curr 'Alice' -->{'Alice':1}
-//2-->acc {'Alice':1}         ## curr 'Bob'    -->{'Alice':1, 'Bob':1}
-//3-->acc {'Alice':1, 'Bob':1}## curr 'Tiff'    -->
 
 
-// var myArray = ["goog", 'aai', "adobe", "cisco", "cisco", "face", "google", "clar"];
-var myArray = [1, 1, 2, 2, 3, 3, 3, 4, 5, 6];
-var a;
-var sum = 0;
-// myArray.sort();
-console.log(myArray);
-const asdf = (ar) => {
-    for (a of ar) {
-        while (ar.includes(a)) {
-            sum++;
-            ar.shift();
-        }
-        console.log('qqq', a, ar)
-        console.log(a, ':', sum);
-        sum = 0;
-    }
-}
-console.log(asdf(myArray));
