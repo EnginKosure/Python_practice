f1de04caf8df53ffa9adb5ec167ccd809b67c861
EnginKosure <engin.kosure@cronos.be>
2020-10-22 21:57:54 +0200

    two solved

--- fzlt54.py
+++ fzlt54.py
@@ -3,3 +3,11 @@
 # and the Bs come last. You can only swap elements of the array.
 # For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'],
 # it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+
+
+def segregate_values(arr):
+    return sorted(arr, reverse=1)
+
+
+x = ['G', 'B', 'R', 'R', 'B', 'R', 'G']
+print(segregate_values(x))
