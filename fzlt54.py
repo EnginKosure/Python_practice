f32fd888cdc6352d7a03f20b81e1567af3c93107
EnginKosure <engin.kosure@cronos.be>
2020-10-23 23:40:31 +0200

    new approach applied

--- fzlt54.py
+++ fzlt54.py
@@ -11,3 +11,11 @@ def segregate_values(arr):
 
 x = ['G', 'B', 'R', 'R', 'B', 'R', 'G']
 print(segregate_values(x))
+
+
+def transform1(arr):
+    for i in range(len(arr)-1):
+        for j in range(i+1, len(arr)):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
