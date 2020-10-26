17475afef5d06838f47ee18e9578fa11623bc350
EnginKosure <engin.kosure@cronos.be>
2020-10-26 22:27:43 +0100

    array approach

--- fzlt57.py
+++ fzlt57.py
@@ -10,3 +10,27 @@ def f_subsets(s):
         result += [j.union({i}) for j in result]
         result.append({i})
     return [{}] + sorted(result, key=len)
+
+
+def findPowerSet(S):
+
+        # N stores total number of subsets
+    N = int(pow(2, len(S)))
+    s = set()
+
+    # generate each subset one by one
+    for i in range(N):
+        # check every bit of i
+        for j in range(len(S)):
+            # if j'th bit of i is set, print S[j]
+            if i & (1 << j):
+                s.add(S[j])
+
+        print(list(s))
+        s.clear()
+
+
+if __name__ == '__main__':
+
+    S = [1, 2, 3]
+    findPowerSet(S)
