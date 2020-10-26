f38a6f52b8bbdad951768b0d4e606baef32c13c5
EnginKosure <engin.kosure@cronos.be>
2020-10-26 22:24:22 +0100

    explore an approach

--- fzlt57.py
+++ fzlt57.py
@@ -2,3 +2,11 @@
 # Write a function that, given a set, generates its power set.
 # For example, given the set {1, 2, 3}, it should return
 # {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+
+
+def f_subsets(s):
+    result = []
+    for i in s:
+        result += [j.union({i}) for j in result]
+        result.append({i})
+    return [{}] + sorted(result, key=len)
