dc86d5062fa9aa2216fa809c0fdc83264d607502
EnginKosure <engin.kosure@cronos.be>
2020-10-28 22:36:46 +0100

    second approach

--- fzlt59.py
+++ fzlt59.py
@@ -30,3 +30,12 @@ def itinerary(flights, myItinerary):
 x = [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')]
 
 print(itinerary(x, "A"))
+
+
+def compute_itenary(lst, start):
+    r = [[i] for i in lst if i[0] == start]
+    for _ in range(len(lst)-1):
+        r = [i + [j]
+             for i in r for j in lst if (lst.count(j) != i.count(j)) and (i[-1][1] == j[0])]
+    r = sorted(r)[0]
+    return [i[0] for i in r] + [r[-1][1]] if r else None
