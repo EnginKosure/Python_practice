4dddb795a9ee061c56c9192ec50f1558873166c4
EnginKosure <engin.kosure@cronos.be>
2020-10-28 22:45:35 +0100

    second approach update

--- fzlt59.py
+++ fzlt59.py
@@ -37,5 +37,5 @@ def compute_itenary(lst, start):
     for _ in range(len(lst)-1):
         r = [i + [j]
              for i in r for j in lst if (lst.count(j) != i.count(j)) and (i[-1][1] == j[0])]
-    r = sorted(r)[0]
-    return [i[0] for i in r] + [r[-1][1]] if r else None
+    r.sort()
+    return [i[0] for i in r[0]] + [r[0][-1][1]] if r else None
