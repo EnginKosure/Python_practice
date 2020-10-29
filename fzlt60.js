75e670b9cfffb3141e72ebb7788b4afd98cf8b79
EnginKosure <engin.kosure@cronos.be>
2020-10-29 03:38:56 +0100

    roman numerals

---
+++ fzlt60.js
@@ -0,0 +1,45 @@
+function romanize(num) {
+    var lookup = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }, roman = '', i;
+    for (i in lookup) {
+        while (num >= lookup[i]) {
+            roman += i;
+            num -= lookup[i];
+        }
+    }
+    console.log(roman);
+    return roman;
+}
+
+romanize(10)
+
+function roman_to_Int(str1) {
+    if (str1 == null) return -1;
+    let num = char_to_int(str1.charAt(0));
+    let pre, curr;
+
+    for (i = 1; i < str1.length; i++) {
+        curr = char_to_int(str1.charAt(i));
+        pre = char_to_int(str1.charAt(i - 1));
+        if (curr <= pre) {
+            num += curr;
+        } else {
+            num = num - pre * 2 + curr;
+        }
+    }
+    return num;
+}
+
+function char_to_int(c) {
+    switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return -1;
+    }
+}
+console.log(roman_to_Int('XXVI'));
+console.log(roman_to_Int('CI'));
