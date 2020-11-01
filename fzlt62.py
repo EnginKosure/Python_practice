e0222c9d42d0a858deebeecee6db6b2f48ee67f9
EnginKosure <engin.kosure@cronos.be>
2020-11-01 22:17:03 +0100

    first try

--- fzlt62.py
+++ fzlt62.py
@@ -1,2 +1,13 @@
 # Using a function rand5() that returns an integer from 1 to 5 (inclusive) with uniform probability,
 # implement a function rand7() that returns an integer from 1 to 7 (inclusive).
+
+from random import randint
+
+
+def rand5():
+    return randint(1, 5)
+
+
+def rand7():
+    x = 5*(rand5()-1) + rand5() - 1
+    return x % 7 + 1 if x < 21 else rand7()
