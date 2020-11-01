d768ee321bc9648418f8d6f30d1495aadbc9fd1b
EnginKosure <engin.kosure@cronos.be>
2020-11-01 22:26:25 +0100

    implement on a ipynb

--- fzlt62.py
+++ fzlt62.py
@@ -19,14 +19,14 @@ def rand7_v2():
     return (rand5() + rand5() + rand5() + rand5()) % 7+1
 
 
-d = {}
-for i in range(100000):
-    # a = (5*(rand5()-1) + rand5() - 1 )%7+1
-    a = (rand5() + rand5() + rand5() + rand5()) % 7+1
-    if a in d:
-        d[a] += 1
-    else:
-        d[a] = 1
-x = dict(sorted(d.items(), key=lambda x: x[0]))
-df3 = pd.DataFrame.from_dict(x, orient="index")
-df3.plot(kind='bar')
+# d = {}
+# for i in range(100000):
+#     # a = (5*(rand5()-1) + rand5() - 1 )%7+1
+#     a = (rand5() + rand5() + rand5() + rand5()) % 7+1
+#     if a in d:
+#         d[a] += 1
+#     else:
+#         d[a] = 1
+# x = dict(sorted(d.items(), key=lambda x: x[0]))
+# df3 = pd.DataFrame.from_dict(x, orient="index")
+# df3.plot(kind='bar')
