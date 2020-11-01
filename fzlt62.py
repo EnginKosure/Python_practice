f03b0884161d9d1b35a4570fde6ce2398df1468a
EnginKosure <engin.kosure@cronos.be>
2020-11-01 22:25:48 +0100

    check

--- fzlt62.py
+++ fzlt62.py
@@ -1,6 +1,8 @@
 # Using a function rand5() that returns an integer from 1 to 5 (inclusive) with uniform probability,
 # implement a function rand7() that returns an integer from 1 to 7 (inclusive).
 
+import pandas as pd
+import random
 from random import randint
 
 
@@ -15,3 +17,16 @@ def rand7():
 
 def rand7_v2():
     return (rand5() + rand5() + rand5() + rand5()) % 7+1
+
+
+d = {}
+for i in range(100000):
+    # a = (5*(rand5()-1) + rand5() - 1 )%7+1
+    a = (rand5() + rand5() + rand5() + rand5()) % 7+1
+    if a in d:
+        d[a] += 1
+    else:
+        d[a] = 1
+x = dict(sorted(d.items(), key=lambda x: x[0]))
+df3 = pd.DataFrame.from_dict(x, orient="index")
+df3.plot(kind='bar')
