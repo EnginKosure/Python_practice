34f59b90eec91724ca483cccf26ef64994235e77
EnginKosure <engin.kosure@cronos.be>
2020-11-05 22:23:11 +0100

    daily py

--- fzlt64.py
+++ fzlt64.py
@@ -1,3 +1,22 @@
 import wikipedia
 result = wikipedia.page('nikola tesla')
 print(result.summary)
+
+
+# Given an array of numbers, find the maximum sum of any contiguous subarray of the array.
+# For example, given the array [34, -50, 42, 14, -5, 86], the maximum sum would be 137,
+# since we would take elements 42, 14, -5, and 86.
+# Given the array [-5, -1, -8, -9], the maximum sum would be 0, since we would not take any elements.
+def f_maxsub(nums):
+    result = float("-inf")
+    if len(nums) < 2:
+        try:
+            result = max(0, nums[0])
+        except:
+            result = 0
+        else:
+            return result
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)+1):
+            result = max(result, sum(nums[i:j]))
+    return max(0, result)
