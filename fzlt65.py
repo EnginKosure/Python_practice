3291d37afca69599dd6457579739180bb9f7b754
EnginKosure <engin.kosure@cronos.be>
2020-11-08 23:31:28 +0100

    unperformant one-liner

--- fzlt65.py
+++ fzlt65.py
@@ -22,3 +22,7 @@ def sum_up2(nums, target):
         if target-nums[i] in d:
             return [d[target-nums[i]], i]
         d[nums[i]] = i
+
+
+def twoSum2(nums, target):
+    return [[i] + [nums.index(target-nums[i])] for i in range(len(nums)) if (target-nums[i] in nums) and nums.index(target-nums[i]) != i][0]
