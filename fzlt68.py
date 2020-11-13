0b427b4bfba721fb4b1a7d2f01001a7d56bf36df
EnginKosure <engin.kosure@cronos.be>
2020-11-13 23:42:28 +0100

    first approach

--- fzlt68.py
+++ fzlt68.py
@@ -2,3 +2,9 @@
 # empty space characters ' ', return the length of last word
 # (last word means the last appearing word if we loop from left to right) in the string.
 # If the last word does not exist, return 0.
+
+def last_length(s):
+    return len(s.split()[-1])
+
+
+print(last_length("One two three"))
