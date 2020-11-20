d8aca967537a3374046a57a632f38dcb0d3e87cb
EnginKosure <engin.kosure@cronos.be>
2020-11-20 22:17:24 +0100

    daily py

---
+++ fzlt69.py
@@ -0,0 +1,13 @@
+# Given a non-empty array of decimal digits representing a non-negative integer,
+# increment one to the integer.
+# The digits are stored such that the most significant digit is at the head of the list,
+# and each element in the array contains a single digit.
+# You may assume the integer does not contain any leading zero,
+# except the number 0 itself.
+
+def incr(arr):
+    print(int((''.join([str(n) for n in arr])))+1)
+    # return int(','.join(arr))+1
+
+
+print(incr([9, 9]))
