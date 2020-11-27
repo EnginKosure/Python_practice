10d678799a54d6e58cf8fd713fe724ecc67886e6
EnginKosure <engin.kosure@cronos.be>
2020-11-27 20:30:44 +0100

    first approach

--- fzlt70.py
+++ fzlt70.py
@@ -8,3 +8,11 @@
 #     26 -> Z
 #     27 -> AA
 #     28 -> AB
+
+
+def convertToTitle(n):
+    result = ""
+    while n > 26:
+        result = chr(65+(n-1) % 26) + result
+        n = (n-1) // 26
+    return chr(64+n) + result
