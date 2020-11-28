c0918e3b6acb91e8fc26be50d59afeccc41fcbb3
EnginKosure <engin.kosure@cronos.be>
2020-11-28 22:01:00 +0100

    first solution

--- fzlt71.py
+++ fzlt71.py
@@ -1,4 +1,3 @@
-# QUESTION: Level of Interview Question = Easy
 # Given an array of size n, find the majority element.
 # The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 # You may assume that the array is non-empty and the majority element always exist in the array.
@@ -6,3 +5,13 @@
 # Output: 3
 # Input: [2,2,1,1,1,2,2]
 # Output: 2
+
+def find_majority(arr):
+    for i in arr:
+        if arr.count(i) > len(arr) / 2:
+            print(i)
+            break
+
+
+find_majority([2, 2, 1, 1, 1, 2, 2])
+find_majority([3, 2, 3])
