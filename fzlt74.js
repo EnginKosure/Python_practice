b328fe6613daa1bb8c2a34decf109ebe1d1d0a79
EnginKosure <engin.kosure@cronos.be>
2020-12-04 00:21:56 +0100

    recursive

--- fzlt74.js
+++ fzlt74.js
@@ -11,11 +11,13 @@ function binaryToDecimal(binaryNumber) {
 }
 // console.log(binaryToDecimal("1111"))
 
-function binary_to_decimal(bstring) {
-    console.log(bstring.slice(-1));
-    if (bstring.length == 0) {
-        return 0
-    }
-    return binary_to_decimal(bstring.slice(0, -1)) * 2 + (bstring.slice(-1) == '1')
-}
-console.log(binary_to_decimal("111"))
+// function binary_to_decimal(bstring) {
+//     console.log(bstring.slice(-1));
+//     if (bstring.length == 0) {
+//         return 0
+//     }
+//     return binary_to_decimal(bstring.slice(0, -1)) * 2 + (bstring.slice(-1) == '1')
+// }
+
+const b_t_d = bs => bs.length == 0 ? 0 : b_t_d(bs.slice(0, -1)) * 2 + (bs.slice(-1) == '1');
+console.log(b_t_d("11"))
