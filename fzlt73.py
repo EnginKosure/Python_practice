192c7a6aa01abf88d5884a3b124607dade07f7dd
EnginKosure <engin.kosure@cronos.be>
2020-12-03 22:53:29 +0100

    second approach

--- fzlt73.py
+++ fzlt73.py
@@ -24,3 +24,18 @@ def happy(n):
 
 print(happy(20))  # False
 print(happy(19))  # True
+
+
+def isHappy(self, n):
+    checked = []
+    nums = str(n)
+    while nums != '1' and not (nums in checked):
+        checked.append(nums)
+        result = 0
+        for num in nums:
+            result += int(num) * int(num)
+        nums = str(result)
+    if (nums == '1'):
+        print("True")
+    else:
+        print("False")
