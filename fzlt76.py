72c446805479b38673f8ac89c8c3de4e642460bf
EnginKosure <engin.kosure@cronos.be>
2020-12-07 22:13:02 +0100

    add round

--- fzlt76.py
+++ fzlt76.py
@@ -1,7 +1,7 @@
 # Write a function that takes coordinates of two points on a two-dimensional plane and
 # returns the length of the line segment connecting those two points.
 def line_length(a1, a2):
-    return ((a1[0]-a2[0])**2+(a1[1]-a2[1])**2)**.5
+    return round(((a1[0]-a2[0])**2+(a1[1]-a2[1])**2)**.5, 2)
 
 
 print(line_length([15, 7], [22, 11]))  # ➞ 8.06
