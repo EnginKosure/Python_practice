50c55f185c74016f955dee605facecb45127780a
EnginKosure <engin.kosure@cronos.be>
2020-12-09 23:35:02 +0100

    daily py

--- fzlt78.py
+++ fzlt78.py
@@ -1,9 +1,15 @@
 # Create a function that takes a list of any length.
 # Modify each element(capitalize, reverse, hyphenate).
+def edit_words(arr):
+    x = [j[::-1] for j in [i.upper() for i in arr]]
+    y = [i[:len(i)//2]+'-'+i[len(i)//2:] for i in x]
+    # s[:4] + '-' + s[4:]
+
+    print(y)
 
 
-# edit_words(["new york city"])  # ["YTIC KR-OY WEN"]
+edit_words(["new york city"])  # ["YTIC KR-OY WEN"]
 
-# edit_words(["null", "undefined"])  # ["LL-UN", "DENIF-EDNU"]
+edit_words(["null", "undefined"])  # ["LL-UN", "DENIF-EDNU"]
 
 # edit_words(["hello", "", "world"])  # ["OLL-EH", "-", "DLR-OW"]
