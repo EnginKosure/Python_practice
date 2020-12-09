422538ead465089844f3a3b4c3a3045b64c21b2c
EnginKosure <engin.kosure@cronos.be>
2020-12-09 23:44:36 +0100

    second approach

--- fzlt78.py
+++ fzlt78.py
@@ -8,6 +8,10 @@ def edit_words(arr):
     print(y)
 
 
+def edit_words1(lst):
+    return [''.join(reversed(x.upper()[0:len(x)//2] + '-' + x.upper()[len(x)//2:])) for x in lst]
+
+
 edit_words(["new york city"])  # ["YTIC KR-OY WEN"]
 
 edit_words(["null", "undefined"])  # ["LL-UN", "DENIF-EDNU"]
