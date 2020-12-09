7cce98bab2ba6e532a5c6afd761296fe9912f3c7
EnginKosure <engin.kosure@cronos.be>
2020-12-09 23:14:47 +0100

    daily py

---
+++ fzlt78.py
@@ -0,0 +1,9 @@
+# Create a function that takes a list of any length.
+# Modify each element(capitalize, reverse, hyphenate).
+
+
+# edit_words(["new york city"])  # ["YTIC KR-OY WEN"]
+
+# edit_words(["null", "undefined"])  # ["LL-UN", "DENIF-EDNU"]
+
+# edit_words(["hello", "", "world"])  # ["OLL-EH", "-", "DLR-OW"]
