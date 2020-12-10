0d73b31c4482c050c0244b9ebdc55e3b96631fb7
EnginKosure <engin.kosure@cronos.be>
2020-12-10 23:06:19 +0100

    first solution

--- fzlt80.py
+++ fzlt80.py
@@ -1,9 +1,13 @@
 # Create a function that takes a list of numbers and returns
 # the sum of all prime numbers in the list.
 
+def sum_primes(arr):
+    return sum([x for x in arr
+                if all(x % y != 0 for y in range(2, x))])
 
-sum_primes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])  # 17
 
-sum_primes([2, 3, 4, 11, 20, 50, 71])  # 87
+print(sum_primes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))  # 17
 
-sum_primes([])  # None
+print(sum_primes([2, 3, 4, 11, 20, 50, 71]))  # 87
+
+print(sum_primes([]))  # None
