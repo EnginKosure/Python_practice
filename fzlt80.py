f5a951f7ddf1293af6f6127707c23d9567704aeb
EnginKosure <engin.kosure@cronos.be>
2020-12-10 22:59:01 +0100

    daily py

---
+++ fzlt80.py
@@ -0,0 +1,9 @@
+# Create a function that takes a list of numbers and returns
+# the sum of all prime numbers in the list.
+
+
+sum_primes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])  # 17
+
+sum_primes([2, 3, 4, 11, 20, 50, 71])  # 87
+
+sum_primes([])  # None
