79463620e2d897108b23b4e7a4957009aa34affe
EnginKosure <engin.kosure@cronos.be>
2020-12-14 22:42:54 +0100

    first unperformant solution

--- fzlt84.py
+++ fzlt84.py
@@ -1,12 +1,19 @@
 # Write a function that returns the length of the shortest contiguous
 # sublist whose sum of all elements strictly exceeds n.
+def min_length(l, n):
+    x = [len(k) if sum(k) > n else -1 for k in [l[i:i+j]
+                                                for i in range(0, len(l)) for j in range(1, len(l)-i+1)]]
+    if all(i == -1 for i in x):
+        return -1
+    else:
+        return sorted(set(x))[1]
 
 
-min_length([5, 8, 2, -1, 3, 4], 9)  # 2
+print(min_length([5, 8, 2, -1, 3, 4], 9))  # 2
 
-min_length([3, -1, 4, -2, -7, 2], 4)  # 3
+print(min_length([3, -1, 4, -2, -7, 2], 4))  # 3
 # Shortest sublist whose sum exceeds 4 is: [3, -1, 4]
 
-min_length([1, 0, 0, 0, 1], 1)  # 5
+print(min_length([1, 0, 0, 0, 1], 1))  # 5
 
-min_length([0, 1, 1, 0], 2)  # -1
+print(min_length([0, 1, 1, 0], 2))  # -1
