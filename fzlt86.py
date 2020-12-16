837bc66396916ab3775289916344a8661a90203f
EnginKosure <engin.kosure@cronos.be>
2020-12-16 23:21:24 +0100

    file read

---
+++ fzlt86.py
@@ -0,0 +1,20 @@
+def find_pass(s):
+    counter = 0
+    x = s.split(':')
+    print('x', x)
+    w = x[0][-1]
+    print(w)
+    exp = x[1]
+    y = x[0].split('-')
+    print('y', y)
+    n1 = int(y[0])
+    z = y[1].split(" ")
+    print('z', z)  # ['11', 't']
+    n2 = int(z[0])
+    if n2 >= exp.count(w) >= n1:
+        counter += 1
+    print(counter)
+    return counter
+
+
+find_pass('2-4 f: cfkmf')
