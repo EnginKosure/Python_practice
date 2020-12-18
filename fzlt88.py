a582e7d0dde3cc6843b01a843a86764b9b896eba
EnginKosure <engin.kosure@cronos.be>
2020-12-18 23:48:02 +0100

    test

--- fzlt88.py
+++ fzlt88.py
@@ -2,11 +2,11 @@ f = open('fzlt88.txt', 'r')
 
 t = 0
 e = 0
-pos = 1
+pos = 0
 for i in f:
     pos += 3
     if pos > len(i):
-        pos = 1
+        pos = 0
         print('t-e', t, e)
     if i[pos] == '#':
         t += 1
