e4294791672e5ea0421c26cb4ecf635af99f9eef
EnginKosure <engin.kosure@cronos.be>
2020-12-18 23:51:49 +0100

    total lines

--- fzlt88.py
+++ fzlt88.py
@@ -3,7 +3,9 @@ f = open('fzlt88.txt', 'r')
 t = 0
 e = 0
 pos = 0
+ln = 0
 for i in f:
+    ln += 1
     pos += 3
     if pos > len(i):
         pos = 0
@@ -12,4 +14,4 @@ for i in f:
         t += 1
     else:
         e += 1
-print(t, e)
+print(t, e, ln)
