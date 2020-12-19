3ce273f2e47949a0f88ab61837e6d2e7efea886f
EnginKosure <engin.kosure@cronos.be>
2020-12-19 22:29:28 +0100

    daily py

--- fzlt89.py
+++ fzlt89.py
@@ -1,6 +1,20 @@
+import re
 
 
-# "PC Files: spotifysetup.exe, dog.jpg"
+def remove_virus(s):
+    # print(s.split())
+    a = s.split()
+    pattern1 = '\**virus*'
+    pattern2 = '\**malware*'
+    for i in a:
+        if re.search(pattern1, i) or re.search(pattern2, i):
+            print(a.index(i))
+            del a[a.index(i)]
+
+    print(a)
+    return a
+
+    # "PC Files: spotifysetup.exe, dog.jpg"
 remove_virus("PC Files: spotifysetup.exe, virus.exe, dog.jpg")
 
 # "PC Files: antivirus.exe, cat.pdf"
