c375a035e2ef0661b6c7bb9d820a5af795186b24
EnginKosure <engin.kosure@cronos.be>
2020-12-22 00:10:51 +0100

    edge case

--- fzlt90.py
+++ fzlt90.py
@@ -1,13 +1,14 @@
 def actual_memory_size(s):
     x = int(s[:-2])*0.93
     if s[-2:] == 'MB':
-        print(str(int(x))+'MB')
+        return str(int(x))+'MB'
+    elif x < 1:
+        return str(int(float(x)*1000))+'MB'
     else:
-        print(str(x)+'GB')
+        return str(x)+'GB'
 
 
-actual_memory_size('32GB')  # 29.76GB
-
-actual_memory_size('2GB')  # 1.86GB
-
-actual_memory_size('512MB')  # 476MB
+print(actual_memory_size('32GB'))  # 29.76GB
+print(actual_memory_size('1GB'))  # 1.86GB
+print(actual_memory_size('2GB'))  # 1.86GB
+print(actual_memory_size('512MB'))  # 476MB
