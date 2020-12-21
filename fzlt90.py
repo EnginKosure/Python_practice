7172dc4964bc3e8cda03d2bcacfb6ac51a63ffca
EnginKosure <engin.kosure@cronos.be>
2020-12-22 00:02:28 +0100

    first solution

--- fzlt90.py
+++ fzlt90.py
@@ -1,5 +1,13 @@
-actual_memory_size(32GB) ➞ 29.76GB
+def actual_memory_size(s):
+    x = int(s[:-2])*0.93
+    if s[-2:] == 'MB':
+        print(str(int(x))+'MB')
+    else:
+        print(str(x)+'GB')
 
-actual_memory_size(2GB) ➞ 1.86GB
 
-actual_memory_size(512MB) ➞ 476MB
+actual_memory_size('32GB')  # 29.76GB
+
+actual_memory_size('2GB')  # 1.86GB
+
+actual_memory_size('512MB')  # 476MB
