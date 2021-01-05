f5150bc339fe078ad727a3316ff6e36854cf02fc
EnginKosure <engin.kosure@cronos.be>
2021-01-05 21:47:36 +0100

    first solution

--- fzlt99.py
+++ fzlt99.py
@@ -1,3 +1,18 @@
+def arithmetic_operation(equation):
+    '''
+    Returns the result of evaluating the string equation
+    '''
+    from operator import add, sub, floordiv, mul
+
+    OPS = {'+': add, '-': sub, '*': mul, '//': floordiv}
+    a, op, b = equation.split()
+
+    try:
+        return OPS[op](int(a), int(b))
+    except ZeroDivisionError:
+        return -1
+
+
 arithmetic_operation("12 + 12")  # 24 // 12 + 12 = 24
 
 arithmetic_operation("12 - 12")  # 24 // 12 - 12 = 0
