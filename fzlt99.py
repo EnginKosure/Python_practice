2e830af5d90a0dafdde0036c4d2d0a1affa10242
EnginKosure <engin.kosure@cronos.be>
2021-01-05 21:51:52 +0100

    with if else cases

--- fzlt99.py
+++ fzlt99.py
@@ -20,3 +20,24 @@ arithmetic_operation("12 - 12")  # 24 // 12 - 12 = 0
 arithmetic_operation("12 * 12")  # 144 // 12 * 12 = 144
 
 arithmetic_operation("12 // 0")  # -1 // 12 / 0 = -1
+
+# Second solution
+OP = {'+': int.__add__, '-': int.__sub__, '*': int.__mul__,
+      '//': lambda a, b: a//b if b else -1}
+
+
+def arithmetic_operation1(n):
+    a, o, b = n.split()
+    return OP[o](int(a), int(b))
+
+
+def arithmetic_operation2(n):
+    a, b, c = n.split()
+    if b == "//":
+        return -1 if c == '0' else int(a)//int(c)
+    elif b == "+":
+        return int(a) + int(c)
+    elif b == "-":
+        return int(a) - int(c)
+    else:
+        return int(a) * int(c)
