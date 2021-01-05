783aea72da22920491ac7dee46aa6a2accd20789
EnginKosure <engin.kosure@cronos.be>
2021-01-05 21:29:59 +0100

    daily py

---
+++ fzlt99.py
@@ -0,0 +1,7 @@
+arithmetic_operation("12 + 12")  # 24 // 12 + 12 = 24
+
+arithmetic_operation("12 - 12")  # 24 // 12 - 12 = 0
+
+arithmetic_operation("12 * 12")  # 144 // 12 * 12 = 144
+
+arithmetic_operation("12 // 0")  # -1 // 12 / 0 = -1
