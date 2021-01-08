c9fad0131a0115889b53bd0de2e7b649f6458ac0
EnginKosure <engin.kosure@cronos.be>
2021-01-08 23:29:57 +0100

    daily py

---
+++ fzlt102.py
@@ -0,0 +1,13 @@
+input = 'abrkaabcdefghijjxxx'
+y = {}
+for i in range(len(input)):
+    counter = 0
+    a = ""
+    for i in range(len(input)):
+        if input[counter] not in a:
+            a = a + input[counter]
+            counter += 1
+    y[a] = len(a)
+    input = input[len(a):]
+b = max(y.keys(), key=lambda k: y[k])
+print(b, len(b))
