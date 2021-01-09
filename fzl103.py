2405d232a174b2a81f47937a46413ca933e374de
EnginKosure <engin.kosure@cronos.be>
2021-01-09 19:25:02 +0100

    solved

--- fzl103.py
+++ fzl103.py
@@ -1,3 +1,19 @@
 # 80 >> 3 = floor(80/2^3) = floor(80/8) = 10
 # -24 >> 2 = floor(-24/2^2) = floor(-24/4) = -6
 # -5 >> 1 = floor(-5/2^1) = floor(-5/2) = -3
+
+def shift_to_right(a, b):
+    print(a//2**b)
+
+
+shift_to_right(80, 3)  # 10
+
+shift_to_right(-24, 2)  # -6
+
+shift_to_right(-5, 1)  # -3
+
+shift_to_right(4666, 6)  # 72
+
+shift_to_right(3777, 6)  # 59
+
+shift_to_right(-512, 10)  # -1
