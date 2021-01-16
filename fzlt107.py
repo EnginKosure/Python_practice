e253f15805b0454d2e37796f8adcf8cee1f82b01
EnginKosure <engin.kosure@cronos.be>
2021-01-16 23:56:49 +0100

    daily py

---
+++ fzlt107.py
@@ -0,0 +1,11 @@
+# 0 to 32 days	Zero
+# 33 to 40 days	SGD$325 per billable day
+# 41 to 48 days	SGD$550 per billable day
+# Greater than 48 days	SGD$600 per billable day
+
+
+bonus(15)  # 0
+
+bonus(37)  # 1625
+
+bonus(50)  # 8200
