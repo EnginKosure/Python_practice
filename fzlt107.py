97f8d7e7a5f015a45ea34fe2db243fa8705c4311
EnginKosure <engin.kosure@cronos.be>
2021-01-17 00:02:16 +0100

    daily py

--- fzlt107.py
+++ fzlt107.py
@@ -3,9 +3,19 @@
 # 41 to 48 days	SGD$550 per billable day
 # Greater than 48 days	SGD$600 per billable day
 
+def bonus(x):
+    if x < 32:
+        return 0
+    elif x < 40:
+        return (x-32)*325
+    elif x < 48:
+        return (x-40)*550
+    else:
+        return (x-48)*600 + (x-40)*550
 
-bonus(15)  # 0
 
-bonus(37)  # 1625
+print(bonus(15))  # 0
 
-bonus(50)  # 8200
+print(bonus(37))  # 1625
+
+print(bonus(50))  # 8200
