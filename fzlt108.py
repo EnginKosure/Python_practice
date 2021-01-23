b688311bfdde32e7c8248af5420c33fac8cd2dab
EnginKosure <engin.kosure@cronos.be>
2021-01-23 23:47:27 +0100

    daily py

--- fzlt108.py
+++ fzlt108.py
@@ -1,12 +1,25 @@
-Create a function that returns the whole of the first sentence which contains a specific word. Include the full stop at the end of the sentence.
+# Create a function that returns the whole of the first sentence
+# which contains a specific word. Include the full stop at the end of the sentence.
 
-Examples
 txt = "I have a cat. I have a mat. Things are going swell."
 
-sentence_searcher(txt, "have") ➞ "I have a cat."
 
-sentence_searcher(txt, "MAT") ➞ "I have a mat."
+def sentence_searcher(t, w):
+    l = t.lower()
+    arr = t.split('.')
+    # print(arr)
+    for i in arr.lower():
+        if i.find(w.lower()) != -1:
+            print(i)
+            return i
+        else:
+            return ''
 
-sentence_searcher(txt, "things") ➞ "Things are going swell."
 
-sentence_searcher(txt, "flat") ➞ ""
+# sentence_searcher(txt, "have")  # "I have a cat."
+
+sentence_searcher(txt, "MAT")  # "I have a mat."
+
+sentence_searcher(txt, "things")  # "Things are going swell."
+
+sentence_searcher(txt, "flat")  # ""
