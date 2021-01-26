92806e18124c03d1899dfb404a927696d97c2b3b
EnginKosure <engin.kosure@cronos.be>
2021-01-27 00:05:20 +0100

    test

--- fzlt111.py
+++ fzlt111.py
@@ -3,21 +3,22 @@ def swap(a, b, c):
     return a + b - c
 
 
+def swap1(a, b, c):
+    print(a ^ b ^ c)
+    return a ^ b ^ c
+
+
 swap(1, 0, 0)  # 1
 # a = 1, b = 0, c = b
 # return a
 
-swap(1, 3, 1)  # 3
+swap1(1, 3, 1)  # 3
 # a = 1, b = 3, c = a
 # return b
 
 swap(27, 31, 31)  # 27
 # a = 27, b = 31, c = b
 # return a
-
-
-def swap1(a, b, c):
-    return a ^ b ^ c
 
 
 def swap2(a, b, c):
