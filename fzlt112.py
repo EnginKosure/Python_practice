35f4dab076d3590c5c5b7cf82789737122f62b76
EnginKosure <engin.kosure@cronos.be>
2021-01-28 22:44:24 +0100

    first solution

--- fzlt112.py
+++ fzlt112.py
@@ -1,8 +1,15 @@
 # Create a function that takes a list lst and a number N and
 # returns a list of two integers from lst whose product equals N.
+def two_product(l, n):
+    x = [int(n/i) for i in l if i != 0]
+    s_x = set(x)
+    s_l = set(l)
+    intersection = s_x.intersection(s_l)
+    return(list(intersection))
 
-two_product([1, 2, -1, 4, 5], 20)  # [4, 5]
 
-two_product([1, 2, 3, 4, 5], 10)  # [2, 5]
+print(two_product([1, 2, -1, 4, 5], 20))  # [4, 5]
 
-two_product([100, 12, 4, 1, 2], 15)  # None
+print(two_product([1, 2, 3, 4, 5], 10))  # [2, 5]
+
+print(two_product([100, 12, 4, 1, 2], 15))  # None
