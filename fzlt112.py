3c88de2615ee7fdd7d52d0cbdfd2918ba67296f2
EnginKosure <engin.kosure@cronos.be>
2021-01-28 22:22:54 +0100

    daily py

---
+++ fzlt112.py
@@ -0,0 +1,8 @@
+# Create a function that takes a list lst and a number N and
+# returns a list of two integers from lst whose product equals N.
+
+two_product([1, 2, -1, 4, 5], 20)  # [4, 5]
+
+two_product([1, 2, 3, 4, 5], 10)  # [2, 5]
+
+two_product([100, 12, 4, 1, 2], 15)  # None
