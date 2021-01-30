3c5b9318e354f730314d03db661145e6159fb993
EnginKosure <engin.kosure@cronos.be>
2021-01-30 23:57:39 +0100

    first solution

--- fzlt113.py
+++ fzlt113.py
@@ -12,3 +12,10 @@
 # mary.lastname ➞ "Major"
 # richard.height ➞ 178
 # giancarlo.nationality ➞ "Italian"
+
+class Employee:
+
+    def __init__(self, full_name, **kwargs):
+        self.full_name = full_name
+        self.name, self.lastname = full_name.split()
+        self.__dict__.update(kwargs)
