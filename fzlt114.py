0e61e8ae33fb84bf718ca920476a9b228a491643
EnginKosure <engin.kosure@cronos.be>
2021-02-01 23:20:36 +0100

    first solution

--- fzlt114.py
+++ fzlt114.py
@@ -1,6 +1,15 @@
 # Write a function that takes an IP address and returns the domain name
 # using PTR DNS records.
 
+import socket
+
+
+def get_domain(x):
+    name, alias, addresslist = socket.gethostbyaddr(x)
+    print(name)
+    return name
+
+
 get_domain("8.8.8.8")  # "dns.google"
 
 get_domain("8.8.4.4")  # "dns.google"
