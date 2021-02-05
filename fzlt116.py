ecfbee7c7ca68cddbe83ac095580316569a114a9
EnginKosure <engin.kosure@cronos.be>
2021-02-05 23:28:43 +0100

    daily py

---
+++ fzlt116.py
@@ -0,0 +1,11 @@
+# Your computer might have been infected by a virus!
+# Create a function that finds the viruses in files and removes them from your computer.
+
+# "PC Files: spotifysetup.exe, dog.jpg"
+remove_virus("PC Files: spotifysetup.exe, virus.exe, dog.jpg")
+
+# "PC Files: antivirus.exe, cat.pdf"
+remove_virus(
+    "PC Files: antivirus.exe, cat.pdf, lethalmalware.exe, dangerousvirus.exe ")
+
+remove_virus("PC Files: notvirus.exe, funnycat.gif")  # "PC Files: notvirus.
