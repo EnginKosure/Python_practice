26790e07b42c7412351a1e4f36aba1250771ce38
EnginKosure <engin.kosure@cronos.be>
2021-02-05 23:30:24 +0100

    first solution

--- fzlt116.py
+++ fzlt116.py
@@ -1,6 +1,15 @@
 # Your computer might have been infected by a virus!
 # Create a function that finds the viruses in files and removes them from your computer.
 
+import re
+
+
+def remove_virus(files):
+    safe = [f for f in files[10:].split(", ") if f and not re.fullmatch(
+        r"(?!not|anti).*(virus|malware)\.exe", f)]
+    return "PC Files: %s" % (", ".join(safe) if safe else "Empty")
+
+
 # "PC Files: spotifysetup.exe, dog.jpg"
 remove_virus("PC Files: spotifysetup.exe, virus.exe, dog.jpg")
 
