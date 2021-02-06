c33490746084e16cbf7d79e08f86f1d3b1852928
EnginKosure <engin.kosure@cronos.be>
2021-02-06 22:22:34 +0100

    second approach

--- fzlt117.py
+++ fzlt117.py
@@ -22,6 +22,11 @@
 
 # Create a function that, given this unique prison arrangement, returns the number of freed prisoners.
 
+
+def freed_prisoners(prison):
+    return sum([1 if prison[i] != prison[i-1] else 0 for i in range(1, len(prison))]) + 1 if prison[0] == 1 else 0
+
+
 freed_prisoners([1, 1, 0, 0, 0, 1, 0])  # 4
 
 freed_prisoners([1, 1, 1])  # 1
