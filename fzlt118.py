b3e0aa5aff8c69ce608843113d950e6203523b40
EnginKosure <engin.kosure@cronos.be>
2021-02-09 23:20:36 +0100

    daily py

--- fzlt118.py
+++ fzlt118.py
@@ -8,3 +8,10 @@ print([1, 2] + [3, 4])
 x = [7, 3, 3, 4, 5]
 x.insert(4, [1, 2])
 print(x)
+
+# Imagine a circle and two squares: a smaller and a bigger one. For the smaller one,
+# the circle is a circumcircle and for the bigger one, an incircle.
+
+
+# Create a function, that takes an integer (radius of the circle) and returns
+# the difference of the areas of the two squares.
