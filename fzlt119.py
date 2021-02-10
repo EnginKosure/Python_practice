dfe047c85ef09ae797eb5a9523a94e7c55bff010
EnginKosure <engin.kosure@cronos.be>
2021-02-10 23:34:29 +0100

    first solution

--- fzlt119.py
+++ fzlt119.py
@@ -1,3 +1,7 @@
+def pluralize(lst):
+    return set(i + 's'*(lst.count(i) > 1) for i in lst)
+
+
 pluralize(["cow", "pig", "cow", "cow"])  # { "cows", "pig" }
 
 pluralize(["table", "table", "table"])  # { "tables" }
