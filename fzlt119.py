8991de64e95c6f5d530d8b7324f30c1d8bcf0200
EnginKosure <engin.kosure@cronos.be>
2021-02-10 23:32:16 +0100

    daily py

---
+++ fzlt119.py
@@ -0,0 +1,5 @@
+pluralize(["cow", "pig", "cow", "cow"])  # { "cows", "pig" }
+
+pluralize(["table", "table", "table"])  # { "tables" }
+
+pluralize(["chair", "pencil", "arm"])  # { "chair", "pencil", "arm" }
