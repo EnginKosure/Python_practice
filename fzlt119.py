2306aed3665a5e14ea22bcda9dd10f8dccb57ae1
EnginKosure <engin.kosure@cronos.be>
2021-02-11 23:18:53 +0100

    daily py

--- fzlt119.py
+++ fzlt119.py
@@ -7,3 +7,15 @@ pluralize(["cow", "pig", "cow", "cow"])  # { "cows", "pig" }
 pluralize(["table", "table", "table"])  # { "tables" }
 
 pluralize(["chair", "pencil", "arm"])  # { "chair", "pencil", "arm" }
+
+
+def arithmetic_operation(s):
+
+
+arithmetic_operation("12 + 12")  # 24 // 12 + 12 = 24
+
+arithmetic_operation("12 - 12")  # 24 // 12 - 12 = 0
+
+arithmetic_operation("12 * 12")  # 144 // 12 * 12 = 144
+
+arithmetic_operation("12 // 0")  # -1 // 12 / 0 = -1
