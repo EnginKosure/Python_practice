3d13c101214f1e5b78af7acd69cd6a18201af193
EnginKosure <engin.kosure@cronos.be>
2021-02-12 23:32:11 +0100

    daily py

---
+++ fzlt120.py
@@ -0,0 +1,8 @@
+reversible_inclusive_list(1, 5)  # [1, 2, 3, 4, 5]
+
+reversible_inclusive_list(2, 8)  # [2, 3, 4, 5, 6, 7, 8]
+
+# [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+reversible_inclusive_list(10, 20)
+
+reversible_inclusive_list(24, 17)  # [24, 23, 22, 21, 20, 19, 18, 17]
