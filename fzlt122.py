b8da4c00f5afffc0401b61979944c9e60093f205
EnginKosure <engin.kosure@cronos.be>
2021-02-16 21:58:52 +0100

    daily py

---
+++ fzlt122.py
@@ -0,0 +1,11 @@
+# Given n pairs of parentheses, write a code to generate all combinations of
+# well-formed (valid for Python) parentheses.
+# For example, given n = 3,
+# a solution set is:
+# [
+#   "((()))",
+#   "(()())",
+#   "(())()",
+#   "()(())",
+#   "()()()"
+# ]
