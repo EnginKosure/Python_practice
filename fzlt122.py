396e7fe0475e66607b01f4abe0cddaa6238eb52d
EnginKosure <engin.kosure@cronos.be>
2021-02-21 23:57:56 +0100

    daily py

--- fzlt122.py
+++ fzlt122.py
@@ -54,3 +54,13 @@ stupid_addition(1, 2)  # "12"
 stupid_addition("1", "2")  # 3
 
 stupid_addition("1", 2)  # None
+
+
+find_vertex(1, 0, 25)  ➞ [0, 25]
+# The vertex of y=x²+25 is at (0, 25).
+
+find_vertex(-1, 0, 25) ➞ [0, 25]
+# The vertex of y=-x²+25 is at (0, 25).
+
+find_vertex(1, 10, 4) ➞ [-5, -21]
+# The vertex of y=x²+10x+4 is at (-5, -21).
