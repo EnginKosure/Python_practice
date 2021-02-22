e9f399cb817715359ed51b964a8bb0889334dd28
EnginKosure <engin.kosure@cronos.be>
2021-02-22 23:40:03 +0100

    second approach

--- fzlt123.py
+++ fzlt123.py
@@ -1,5 +1,8 @@
 # Create a function that takes the side n of the game screen and returns the number of times the snake can eat
 # before it runs out of space in the game screen.
+from math import floor, log2
+
+
 def snakefill(n):
     return len(bin(n**2))-3
 
@@ -9,3 +12,7 @@ snakefill(3)  # 3
 snakefill(6)  # 5
 
 snakefill(24)  # 9
+
+
+def snakefill2(n):
+    return floor(log2(n*n))
