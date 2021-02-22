5f78c678b9848e73abeaf608c265391a51324e2f
EnginKosure <engin.kosure@cronos.be>
2021-02-22 23:37:33 +0100

    daily py

---
+++ fzlt123.py
@@ -0,0 +1,8 @@
+# Create a function that takes the side n of the game screen and returns the number of times the snake can eat
+# before it runs out of space in the game screen.
+
+snakefill(3)  # 3
+
+snakefill(6)  # 5
+
+snakefill(24)  # 9
