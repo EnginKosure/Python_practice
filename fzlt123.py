0a448bb067a0299658e48c6b4bd52b9d67dda996
EnginKosure <engin.kosure@cronos.be>
2021-02-24 22:45:35 +0100

    try wikipedia for python

--- fzlt123.py
+++ fzlt123.py
@@ -1,5 +1,6 @@
 # Create a function that takes the side n of the game screen and returns the number of times the snake can eat
 # before it runs out of space in the game screen.
+import wikipedia
 import pywhatkit as kit
 from math import floor, log2
 
@@ -19,4 +20,9 @@ def snakefill2(n):
     return floor(log2(n*n))
 
 
-kit.sendwhatmsg('+3**********', 'test with python', 22, 48)
+# kit.sendwhatmsg('+3**********', 'test with python', 22, 48)
+
+wiki_val1 = wikipedia.search("Barack Obama")
+print(wiki_val1)
+wiki_val2 = wikipedia.search("Ford", results=3)
+print(wiki_val2)
