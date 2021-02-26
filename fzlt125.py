0ddb6d6898670f19f7f7aedafa4923770eaa104c
EnginKosure <engin.kosure@cronos.be>
2021-02-26 23:31:35 +0100

    daily py

---
+++ fzlt125.py
@@ -0,0 +1,21 @@
+import random
+
+answer = random.randint(1, 101)
+
+max = 7
+counter = 0
+
+question = 'I can find it in max {} times : '.format(max)
+print("Let's play the guessing game!")
+
+while True:
+    guess = int(input(question))
+    if guess < answer:
+        counter += 1
+        print('Little higher, I have still ', (max - counter), 'chances.')
+    elif guess > answer:
+        counter += 1
+        print('Little lower, I have still ', (max - counter), 'chances.')
+    else:
+        print('As I said I find it in ', counter, ' times')
+        break
