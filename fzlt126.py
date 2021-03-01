f93f929a40277b15e8e8ea3a21c827de23d67249
EnginKosure <engin.kosure@cronos.be>
2021-03-01 22:45:22 +0100

    second approach

--- fzlt126.py
+++ fzlt126.py
@@ -33,3 +33,16 @@ def takeSecond(elem):
     M = sorted([i[0] for i in P if i[1] == K[1]])
     for j in M:
         print(j)
+
+
+if __name__ == '__main__':
+    students =[]
+    scores = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+    scores = [i[1] for i in students]
+    scores = list(sorted(set(scores)))
+    res=sorted([i[0] for i in students if i[1]==scores[1]])
+    print(*res,sep="\n")
