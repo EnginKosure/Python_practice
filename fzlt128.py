6ef169341167c3668c59e230ea8638fd56ea5e80
EnginKosure <engin.kosure@cronos.be>
2021-03-04 22:59:50 +0100

    test the solution

--- fzlt128.py
+++ fzlt128.py
@@ -4,3 +4,13 @@ class Employee:
         self.lastname = lastname
         self.fullname = firstname + ' ' + lastname
         self.email = '{}.{}@company.com'.format(firstname, lastname).lower()
+
+
+emp_1 = Employee("John", "Smith")
+emp_2 = Employee("Mary",  "Sue")
+emp_3 = Employee("Antony", "Walker")
+emp_1.fullname  # "John Smith"
+
+emp_2.email  # "mary.sue@company.com"
+
+emp_3.firstname  # "Antony"
