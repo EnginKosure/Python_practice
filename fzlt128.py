e47f581efcaaec3a61144af7e8cbbc997e992bbd
EnginKosure <engin.kosure@cronos.be>
2021-03-04 23:02:25 +0100

    format the string

--- fzlt128.py
+++ fzlt128.py
@@ -2,15 +2,15 @@ class Employee:
     def __init__(self, firstname, lastname):
         self.firstname = firstname
         self.lastname = lastname
-        self.fullname = firstname + ' ' + lastname
+        self.fullname = f'{firstname} {lastname}'
         self.email = '{}.{}@company.com'.format(firstname, lastname).lower()
 
 
 emp_1 = Employee("John", "Smith")
 emp_2 = Employee("Mary",  "Sue")
 emp_3 = Employee("Antony", "Walker")
-emp_1.fullname  # "John Smith"
+print(emp_1.fullname)  # "John Smith"
 
-emp_2.email  # "mary.sue@company.com"
+print(emp_2.email)  # "mary.sue@company.com"
 
 emp_3.firstname  # "Antony"
