faf0dcf44856e7667d93384bd81def06840345f5
EnginKosure <engin.kosure@cronos.be>
2021-03-05 23:59:32 +0100

    recap

--- fzlt129.py
+++ fzlt129.py
@@ -3,3 +3,11 @@ def find_odd(s):
 
 
 print(find_odd([1, 2, 3, 4, 5, 6]))
+
+
+def question_marks(s):
+    for k, v in enumerate(s):
+        print(k, v)
+
+
+question_marks('acc?7??sss?3rr1??????5"')
