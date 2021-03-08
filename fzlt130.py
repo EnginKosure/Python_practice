ae80c8592a9c1eb52e7f5131c8ac372df286b461
EnginKosure <engin.kosure@cronos.be>
2021-03-08 23:57:07 +0100

    refactor

--- fzlt130.py
+++ fzlt130.py
@@ -1,4 +1,8 @@
 a_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-low = 3
-high = 7
-filter(lambda x: low <= x < high, a_list)
+
+
+def within_bounds(value, low=3, high=7):
+    return low <= value < high
+
+
+filter(within_bounds, a_list)
