dc7cb39380f7081324265e19596221286c40b1cd
EnginKosure <engin.kosure@cronos.be>
2021-03-23 21:51:04 +0100

    test the solution

--- fzlt134.py
+++ fzlt134.py
@@ -1,2 +1,12 @@
+# def pageCount(n, p):
+#     return min(p//2, max((n % 2 == 0)((n-p-1)//2 + 1), (n % 2 != 0)(n-p)//2))
+
+
 def pageCount(n, p):
-    return min(p//2, max((n % 2 == 0)((n-p-1)//2 + 1), (n % 2 != 0)(n-p)//2))
+    if n % 2 == 0 and n-1 == p:
+        return min(p//2, 1)
+    else:
+        return min(p//2, (n-p)//2)
+
+
+print(pageCount(5, 4))
