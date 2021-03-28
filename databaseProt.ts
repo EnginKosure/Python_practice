3a952c206f4893996dee6da50432ee2c2ef935da
EnginKosure <engin.kosure@cronos.be>
2021-03-28 23:26:05 +0200

    add main spec

--- databaseProt.ts
+++ databaseProt.ts
@@ -3,4 +3,10 @@ export class TemplateType1{
     name:string;
     description:string;
     subMenu:string;
+    templateType:string;
+    pictures:ImageFile[];
+    unitPrice:number;
+    letterSpace?:boolean;
+    colors?:Color[];
+    bgColors?:Color[];
 }
