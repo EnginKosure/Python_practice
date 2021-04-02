fb6514be41dcc4576eff81ddc1504a69570283d9
EnginKosure <engin.kosure@cronos.be>
2021-04-02 23:22:11 +0200

    explore unittest lib

---
+++ test_fzlt139.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class PhoneBookTest(unittest.TestCase):
+
+    def test_lookup_by_name(self):
+        phonebook = PhoneBook()
+        phonebook.add("Bob", "12345")
+        number = phonebook.lookup("Bob")
+        self.assertEqual("12345", number)
