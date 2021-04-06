b368fb6b4aaf0bf4896b9fe93b8db4ac8385fc1f
EnginKosure <engin.kosure@cronos.be>
2021-04-06 08:36:34 +0200

    assertIn implementation

--- test_fzlt139.py
+++ test_fzlt139.py
@@ -5,18 +5,31 @@ from fzlt139 import PhoneBook
 
 class PhoneBookTest(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.phonebook = PhoneBook()
+
+    # def tearDown(self) -> None:
+    #     return super().tearDown()
+
     def test_lookup_by_name(self):
-        phonebook = PhoneBook()
-        phonebook.add("Bob", "12345")
-        number = phonebook.lookup("Bob")
+        self.phonebook.add("Bob", "12345")
+        number = self.phonebook.lookup("Bob")
         self.assertEqual("12345", number)
 
     def test_missing_name(self):
-        phonebook = PhoneBook()
         with self.assertRaises(KeyError):
-            phonebook.lookup("missing")
+            self.phonebook.lookup("missing")
 
-    @unittest.skip("WIP")
+    # @unittest.skip("WIP")
     def test_is_consistent(self):
-        phonebook = PhoneBook()
-        self.assertTrue(phonebook.is_consistent())
+        self.phonebook.add("Bob", "12345")
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Anna", "012345")
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Sue", "12345")
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_add_method(self):
+        self.phonebook.add("Sue", "123433")
+        self.assertIn("Sue", self.phonebook.get_names())
+        self.assertIn("123433", self.phonebook.get_numbers())
