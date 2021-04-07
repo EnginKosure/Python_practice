424e4463f3c0d23ac3195fb9302e2a7c332222ad
EnginKosure <engin.kosure@cronos.be>
2021-04-07 23:29:16 +0200

    prototype usage

--- fzltjs3.js
+++ fzltjs3.js
@@ -6,7 +6,7 @@ function Book(title, author, ISBN, numCopies) {
 }
 // Here the function is declared on prototype, because we don't need to create a new instance
 // of this function each time we make a new object.
-// Each instance can access this function from its prototype and doesn't create a new one each time...
+// Each instance can access this function and use it from its prototype and doesn't create a new one each time...
 Book.prototype.getAvailability = function () {
     if (this.numCopies == 0) {
         return "Out of stock";
@@ -15,3 +15,18 @@ Book.prototype.getAvailability = function () {
     }
     return "In stock";
 }
+
+Book.prototype.sell = function (numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+}
+
+Book.prototype.restock = function (numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+}
+
+const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+console.log(HungerGames.getAvailability());
+HungerGames.restock(12);
+console.log(HungerGames.getAvailability());
+HungerGames.sell(17);
+console.log(HungerGames.getAvailability());
