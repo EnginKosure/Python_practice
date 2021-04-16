bb4793f8673e39c4cfe138670bec4574f605a747
EnginKosure <engin.kosure@cronos.be>
2021-04-16 04:40:22 +0200

    another approach with split

--- fzlt146.py
+++ fzlt146.py
@@ -22,12 +22,26 @@ def isPhoneNumber1(text):
     arr_text = text.split('-')
     if len(text) != 12 and len(arr_text) != 3:
         return False  # not phone number-sized
-    for i in arr_text:
-        for j in i:
-            if not j.isdecimal():
-                return False
+    if len([False for i in arr_text for j in i if not j.isdecimal()]) > 0:
+        return False
+    # for i in arr_text:
+    #     for j in i:
+    #         if not j.isdecimal():
+    #             return False
     return True  # "text" is a phone number!
 
 
 print(isPhoneNumber1('415-555-4242'))
 print(isPhoneNumber1('Mos-him-mosh'))
+
+print(isPhoneNumber1('416-555-4242'))
+a = {
+    1: [{'n': 1}, {'n': 2}],
+    2: [{'n': 3}, {'n': 4}],
+    3: [{'n': 5}],
+}
+good = [1, 2]
+[r['n'] for g in good for r in a[g]]
+[1, 2, 3, 4]
+
+print([r['n'] for g in good for r in a[g]])
