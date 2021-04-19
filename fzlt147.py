0a9e89a0fe7e8ccd379664cc0482fffb5d3b7dc0
EnginKosure <engin.kosure@cronos.be>
2021-04-19 17:39:31 +0200

    with multiple params

--- fzlt147.py
+++ fzlt147.py
@@ -1,7 +1,7 @@
 def operations(x, y):
-    return x+y, x*y
+    return x+y, x*y, '3th param'
 
 
-res1, res2 = operations(4, 4)
+res1, res2, res3 = operations(4, 4)
 
-print(res1, res2)
+print(res1, res2, res3[:3])
