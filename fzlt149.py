b53cd5eea39366a211a570fcd283bbcd5b5f1f61
EnginKosure <engin.kosure@cronos.be>
2021-04-23 23:33:53 +0200

    close connection

--- fzlt149.py
+++ fzlt149.py
@@ -6,7 +6,8 @@ try:
     print("\nDatabase created and connected to SQLite")
     sqlite_select_Query = "select sqlite_version();"
     conn.execute(sqlite_select_Query)
-
-
+    record = conn.fetchall()
+    print("\nSQLite Database Version is: ", record)
+    conn.close()
 except sqlite3.Error as error:
     print("\nError while connecting to sqlite", error)
