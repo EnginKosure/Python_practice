513771c33a39e3f7f0da9161a12c1ddb2ac4ec92
EnginKosure <engin.kosure@cronos.be>
2021-04-23 23:31:15 +0200

    execute select st

--- fzlt149.py
+++ fzlt149.py
@@ -4,6 +4,9 @@ try:
     sqlite_Connection = sqlite3.connect('temp.db')
     conn = sqlite_Connection.cursor()
     print("\nDatabase created and connected to SQLite")
+    sqlite_select_Query = "select sqlite_version();"
+    conn.execute(sqlite_select_Query)
+
 
 except sqlite3.Error as error:
     print("\nError while connecting to sqlite", error)
