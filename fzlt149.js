42beb8c59933c041aeb67896f57d2c52346905d1
EnginKosure <engin.kosure@cronos.be>
2021-04-24 14:09:05 +0200

    add comments

--- fzlt149.js
+++ fzlt149.js
@@ -1,3 +1,7 @@
+//Extending buit-in functions are considered bad-practice.
+//Because it makes the buit-in function heavier and if any name conflict appears, it will crash.
+//For a better way, see fzlt140.js
+
 //Custom average function
 Array.prototype.average = function () {
     return this.reduce((acc, elem) => acc + elem) / this.length;
