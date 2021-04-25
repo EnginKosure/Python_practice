27ca5b1ed83ac062ac3b043ba0e3e296362365f4
EnginKosure <engin.kosure@cronos.be>
2021-04-25 23:28:42 +0200

    first solution

--- fzlt150.py
+++ fzlt150.py
@@ -2,3 +2,11 @@
 # Your code should find the biggest number of consecutive zeros in the string. For example,
 # given the string:"1001101000110"
 # The biggest number of consecutive zeros is 3.
+def consecutive(s):
+    # print(max([len(i) for i in s.split("1")]))
+    return max([len(i) for i in s.split("1")])
+
+
+t = "1001101000110"
+
+consecutive(t)
