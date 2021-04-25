b3b74ac6646843263ac0c248d65b4f14540ec325
EnginKosure <engin.kosure@cronos.be>
2021-04-25 23:23:05 +0200

    daily py

---
+++ fzlt150.py
@@ -0,0 +1,4 @@
+# The goal of this challenge is to analyze a binary string consisting of only zeros and ones.
+# Your code should find the biggest number of consecutive zeros in the string. For example,
+# given the string:"1001101000110"
+# The biggest number of consecutive zeros is 3.
