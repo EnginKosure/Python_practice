a3e92b0269db2c354caa9c00df60368bf5cd645d
EnginKosure <engin.kosure@cronos.be>
2021-04-27 23:20:02 +0200

    refactor

--- fzlt152.py
+++ fzlt152.py
@@ -1,8 +1,6 @@
-import random
+from random import randint
 heads = 0
 for i in range(1, 1001):
-    if random.randint(0, 1) == 1:
+    if randint(0, 1) == 1:
         heads += 1
-    if i == 500:
-        print('Halfway done!')
 print('Heads came up ' + str(heads) + ' times.')
