24a30625fe154f98629f7d61d4a5181bd7fb9f09
EnginKosure <engin.kosure@cronos.be>
2021-04-29 20:35:08 +0200

    explore currying

--- fzlt153.js
+++ fzlt153.js
@@ -2,8 +2,8 @@
 const add = (a, b) => a + b
 const curriedAdd = a => b => add(a, b)
 
-const addTwo = curriedAdd(2)
+const addTwo = curriedAdd(6)
 
-addTwo(2) // 4
-addTwo(4) // 6
-console.log(addTwo(4));
+addTwo(2) // 8
+addTwo(4) // 10
+console.log(addTwo(4));//10
