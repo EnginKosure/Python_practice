1748b4f58bc69b59dc4af81b454c25b6a3f3176b
EnginKosure <engin.kosure@cronos.be>
2021-04-30 23:15:01 +0200

    explore currying

--- fzlt153.js
+++ fzlt153.js
@@ -7,3 +7,20 @@ const addTwo = curriedAdd(6)
 addTwo(2) // 8
 addTwo(4) // 10
 console.log(addTwo(4));//10
+
+function curry(f) { // curry(f) does the currying transform
+    return function (a) {
+        return function (b) {
+            return f(a, b);
+        };
+    };
+}
+
+// usage
+function sum(a, b) {
+    return a + b;
+}
+
+let curriedSum = curry(sum);
+
+console.log(curriedSum(1)(2)); // 3
