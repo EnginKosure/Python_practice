4070fcd388682aa2ce04b9d9f7e359ddef68da73
EnginKosure <engin.kosure@cronos.be>
2021-05-02 23:27:42 +0200

    daily py

---
+++ fzlt154.py
@@ -0,0 +1,2 @@
+def is_merge(s, part1, part2):
+    return (s[0] == part1[0] and is_merge(s[1:], part1[1:], part2) or s[0] == part2[0] and is_merge(s[1:], part1, part2[1:]) if (s and part1 and part2) else s == part1 + part2)
