6bc0672be15153827a6dcca052ce39d275eb9c97
EnginKosure <engin.kosure@cronos.be>
2021-05-08 06:13:43 +0200

    nested arr navigation

---
+++ fzlt157.py
@@ -0,0 +1,33 @@
+tm = [
+    ["X", "W3", "E2", "S3", "S4"],
+    ["S1", "E1", "N1", "S2", "W1"],
+    ["S2", "E1", "N2", "S1", "N3"],
+    ["N1", "X", "S2", "E1", "W4"],
+    ["X", "E3", "X", "N2", "W4"]
+]
+
+
+def find_x(tm):
+    mid = len(tm)//2
+    rp = mid
+    cp = mid
+    pos = tm[rp][cp]
+    # for i in tm:
+    #     for j in i:
+    #         if j[0] == 'N':
+
+    def go_to(r, c):
+        pos = tm[r, c]
+
+    if pos[0] == 'N':
+        rp -= int(pos[1])
+        pos = go_to(rp, cp)
+    elif pos[0] == 'S':
+        rp += int(pos[1])
+        pos = go_to(rp, cp)
+    elif pos[0] == 'E':
+        cp += int(pos[1])
+        pos = go_to(rp, cp)
+    elif pos[0] == 'w':
+        cp -= int(pos[1])
+        pos = go_to(rp, cp)
