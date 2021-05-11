8bcf9306530bbb69853f4be021ea7edb608a1576
EnginKosure <engin.kosure@cronos.be>
2021-05-11 22:55:55 +0200

    simple string expansion

---
+++ fzlt160.py
@@ -0,0 +1,4 @@
+# Given a string, return the expansion of that string.
+
+# Input will consist of only lowercase letters and numbers (1 to 9) in valid parenthesis.
+# There will be no letters or numbers after the last closing parenthesis.
