2f9873e69ddb8eb1554ca716440f79bba7cf5e33
EnginKosure <engin.kosure@cronos.be>
2021-05-16 00:32:55 +0200

    for duplicates

--- fzlt163.py
+++ fzlt163.py
@@ -1,14 +1,14 @@
 # Given three arrays of integers, return the sum of elements that are common in all three arrays.
 
-# For example:
-
-
-# More examples in the test cases.
 
 def common(a, b, c):
-    return sum(set(a).intersection(set(b)).intersection(set(c)))
+    out = [x for x in a if x in b and x in c]
+    print(sum(out))
+    return sum(set(a).intersection(set(b), set(c)))
 
 
 # = 5 because 2 & 3 are common in all 3 arrays
-common([1, 2, 3], [5, 3, 2], [7, 3, 2])
-# common([1,2,2,3],[5,3,2,2],[7,3,2,2]) = 7 because 2,2 & 3 are common in the 3 arrays
+# common([1, 2, 3], [5, 3, 2], [7, 3, 2])
+# print(common([1, 2, 3], [5, 3, 2], [7, 3, 2]))
+# = 7 because 2,2 & 3 are common in the 3 arrays
+common([1, 2, 2, 3], [5, 3, 2, 2], [7, 3, 2, 2])
