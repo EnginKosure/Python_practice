38134ce4ff3b468e842c66e9a22746187d578775
EnginKosure <engin.kosure@cronos.be>
2021-05-17 22:42:59 +0200

    daily py

--- fzlt163.py
+++ fzlt163.py
@@ -9,6 +9,6 @@ def common(a, b, c):
 
 # = 5 because 2 & 3 are common in all 3 arrays
 # common([1, 2, 3], [5, 3, 2], [7, 3, 2])
-# print(common([1, 2, 3], [5, 3, 2], [7, 3, 2]))
+print(common([1, 2, 3], [5, 3, 2], [7, 3, 2]))
 # = 7 because 2,2 & 3 are common in the 3 arrays
 common([1, 2, 2, 3], [5, 3, 2, 2], [7, 3, 2, 2])
