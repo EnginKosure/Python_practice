195a0777d4fa1dead8f7b8144790dc1200b3a2e4
EnginKosure <engin.kosure@cronos.be>
2021-05-19 23:33:38 +0200

    daily py

---
+++ fzlt165.py
@@ -0,0 +1,7 @@
+# All integers can be uniquely expressed as a sum of powers of 3 using each power of 3 at most once.
+
+# For example:
+
+# 17 = (-1) +   0  + (-9) + 27 = (-1 * 3^0) + ( 0 * 3^1) + (-1 * 3^2) + (1 * 3^3)
+# -8 =   1  +   0  + (-9)      = ( 1 * 3^0) + ( 0 * 3^1) + (-1 * 3^2)
+# 25 =   1  + (-3) +   0  + 27 = ( 1 * 3^0) + (-1 * 3^1) + ( 0 * 3^2) + (1 * 3^3)
