a753362ac39e79024bd9e7825a8f465395cbddeb
EnginKosure <engin.kosure@cronos.be>
2021-05-22 17:23:25 +0200

    refactor

--- fzlt166.py
+++ fzlt166.py
@@ -1,10 +1,12 @@
 def first_non_consecutive(arr):
-    return [arr[i] for i in range(1, len(arr)) if abs(arr[i] - arr[i-1]) > 1][0]
+    l = [arr[i] for i in range(1, len(arr)) if abs(arr[i] - arr[i-1]) > 1]
+    return l[0] if len(l) > 0 else None
+    # return [arr[i] for i in range(1, len(arr)) if abs(arr[i] - arr[i-1]) > 1][0]
     # for i in range(1, len(arr)):
     #     if abs(arr[i] - arr[i-1]) > 1:
     #         return arr[i]
     # return None
 
 
-a = [1, 2, 3, 4, 6, 7, 8]
+a = [1, 2, 3, 4, 5, 6, 7, 8]
 print(first_non_consecutive(a))
