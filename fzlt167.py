974ec022d3ecf484de81e28f21539d552207ae99
EnginKosure <engin.kosure@cronos.be>
2021-05-23 22:56:19 +0200

    first solution

--- fzlt167.py
+++ fzlt167.py
@@ -2,3 +2,8 @@
 # You must check that all values in the array are
 # below or equal to the limit value. If they are,
 # return true. Else, return false.
+def small_enough(array, limit):
+    return all([x <= limit for x in array])
+
+
+print(small_enough([101, 45, 75, 105, 99, 107], 107))
