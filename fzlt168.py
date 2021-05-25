8c4352e96c08241c3692b56373361376b37d3191
EnginKosure <engin.kosure@cronos.be>
2021-05-25 23:57:48 +0200

    refactor

--- fzlt168.py
+++ fzlt168.py
@@ -6,8 +6,7 @@ result = dict()
 for i in x:  # O(N)
     if i in result:  # O(1)
         result[i] += 1
-    else:
-        result[i] = 1
+    result[i] = 1
 
 for i in y:  # O(N)
     if i in result:  # O(1)
