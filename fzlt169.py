87ba67006124fbe568bb21b193aed5caf15634e5
EnginKosure <engin.kosure@cronos.be>
2021-05-27 21:38:32 +0200

    test

--- fzlt169.py
+++ fzlt169.py
@@ -7,3 +7,11 @@ def lowest(s):
     if not s:
         return 0
     return int(len(s) / cnt) - (int((len(s) / cnt) % 2) == 0) if cnt else len(s)
+
+
+a = "aebecda"  # ->  "aba | c | ede"      ->  lowest(s) = 1
+b = "eutxutuatgextu"  # ->  "xttattx | uuegeuu"  ->  lowest(s) = 7
+c = "abbddc"  # ->  "bab | dcd"          ->  lowest(s) = 3
+d = "abcd"  # ->  "a | b | c | d"      ->  lowest(s) = 1
+
+print(lowest(b))
