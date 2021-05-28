cdd1a2de5176b53cd9b55bc2e0045e422e3028b1
EnginKosure <engin.kosure@cronos.be>
2021-05-28 21:57:45 +0200

    basic solution

--- fzlt170.py
+++ fzlt170.py
@@ -1,2 +1,12 @@
 # Write function RemoveExclamationMarks which removes
 # all exclamation marks from a given string.
+def remove_exclamation_marks(s):
+    new_s = ''
+    for i in s:
+        if i == "!":
+            continue
+        new_s += i
+    return new_s
+
+
+print(remove_exclamation_marks("Hello World!!!"))
