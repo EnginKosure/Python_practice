3f9c77221e25a0e4f28c514bad74b81f60677139
EnginKosure <engin.kosure@cronos.be>
2021-06-03 21:45:13 +0200

    test

--- fzlt175.py
+++ fzlt175.py
@@ -4,3 +4,7 @@ def amort(rate, bal, term, num_payments):
     for _ in range(1, num_payments):
         bal -= (c - r * bal)
     return f"num_payment {num_payments} c {round(c)} princ {round(c - r * bal)} int {round(r * bal)} balance {round(bal - (c - r * bal))}"
+
+
+# "num_payment 20 c 459 princ 445 int 14 balance 1809"
+print(amort(7.4, 10215, 24, 20))
