a1142a151771332a1ae1cfb02f74e5993143df20
EnginKosure <engin.kosure@cronos.be>
2021-06-04 23:24:01 +0200

    second approach

--- fzlt176.py
+++ fzlt176.py
@@ -1,2 +1,10 @@
 def not_visible_cubes(n):
     return (n - 2) ** 3 if n > 2 else 0
+
+
+def not_visible_cubes1(n):
+    return max(n - 2, 0) ** 3
+
+
+def not_visible_cubes2(n):
+    return n > 2 and (n-2)**3
