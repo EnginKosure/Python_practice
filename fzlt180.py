c6ff02d886c5d37eacff8d9edd95a80577255ad0
EnginKosure <engin.kosure@cronos.be>
2021-06-09 22:31:34 +0200

    daily py

--- fzlt180.py
+++ fzlt180.py
@@ -1,3 +1,10 @@
 a = [1, 2, 3, 4, 5]
 del a[::2]
 print(a)
+
+
+b = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+transpose = [list(i) for i in zip(*b)]
+print(transpose)  # [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
