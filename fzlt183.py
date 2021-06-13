6059d076455b9cde4f20acefb7d422b98a6a23c5
EnginKosure <engin.kosure@cronos.be>
2021-06-13 23:03:16 +0200

    daily py

--- fzlt183.py
+++ fzlt183.py
@@ -1,8 +1,21 @@
 list1 = [
-  { 'firstName': 'Harry', 'lastName': 'K.', 'country': 'Brazil', 'continent': 'Americas', 'age': 22, 'language': 'JavaScript', 'githubAdmin': 'yes' },
-  { 'firstName': 'Kseniya', 'lastName': 'T.', 'country': 'Belarus', 'continent': 'Europe', 'age': 49, 'language': 'Ruby', 'githubAdmin': 'no' },
-  { 'firstName': 'Jing', 'lastName': 'X.', 'country': 'China', 'continent': 'Asia', 'age': 34, 'language': 'JavaScript', 'githubAdmin': 'yes' },
-  { 'firstName': 'Piotr', 'lastName': 'B.', 'country': 'Poland', 'continent': 'Europe', 'age': 128, 'language': 'JavaScript', 'githubAdmin': 'no' }
-  ]
+    {'firstName': 'Harry', 'lastName': 'K.', 'country': 'Brazil',
+        'continent': 'Americas', 'age': 22, 'language': 'JavaScript', 'githubAdmin': 'yes'},
+    {'firstName': 'Kseniya', 'lastName': 'T.', 'country': 'Belarus',
+        'continent': 'Europe', 'age': 49, 'language': 'Ruby', 'githubAdmin': 'no'},
+    {'firstName': 'Jing', 'lastName': 'X.', 'country': 'China', 'continent': 'Asia',
+        'age': 34, 'language': 'JavaScript', 'githubAdmin': 'yes'},
+    {'firstName': 'Piotr', 'lastName': 'B.', 'country': 'Poland',
+        'continent': 'Europe', 'age': 128, 'language': 'JavaScript', 'githubAdmin': 'no'}
+]
 
-  
+
+def find_admin(lst, lang):
+    admins = []
+    for i in lst:
+        if i['language'] == lang and i['githubAdmin'] == 'yes':
+            admins.append(i['firstName'])
+    return admins
+
+
+print(find_admin(list1, 'JavaScript'))
