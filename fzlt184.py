444434fd0ce80d1638dc4325cf0b8d9ed9b86c11
EnginKosure <engin.kosure@cronos.be>
2021-06-15 22:59:09 +0200

    daily python

--- fzlt184.py
+++ fzlt184.py
@@ -1,2 +1,10 @@
+from math import ceil
 n = 6
 print('\n'.join('* ' * i for i in range(1, n + 1)))
+
+
+def new_avg(arr, newavg):
+    next = ceil(newavg * (len(arr) + 1) - sum(arr))
+    if next <= 0:
+        raise ValueError
+    return next
