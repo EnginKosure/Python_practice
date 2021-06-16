971b4062fa39f27464606ba6f34a351a9753ee3e
EnginKosure <engin.kosure@cronos.be>
2021-06-16 23:10:00 +0200

    daily python

--- fzlt184.py
+++ fzlt184.py
@@ -1,3 +1,4 @@
+import re
 from math import ceil
 n = 6
 print('\n'.join('* ' * i for i in range(1, n + 1)))
@@ -8,3 +9,6 @@ def new_avg(arr, newavg):
     if next <= 0:
         raise ValueError
     return next
+
+
+len(re.findall('python', 'python is a programming language. python is python.'))
