70cf9114cf458cbf1a2d153e3960a19e7b94af77
EnginKosure <engin.kosure@cronos.be>
2021-07-05 23:48:25 +0200

    daily py

--- fzlt188.py
+++ fzlt188.py
@@ -11,3 +11,22 @@ def Ordered_binary_Search(olist, item):
                 return binarySearch(olist[:midpoint], item)
             else:
                 return binarySearch(olist[midpoint+1:], item)
+
+
+def binarySearch(alist, item):
+
+    first = 0
+    last = len(alist) - 1
+    found = False
+
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if alist[midpoint] == item:
+            found = True
+        else:
+            if item < alist[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+
+    return found
