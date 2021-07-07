044fc5f74ffd1693c0833f978bb75b75255bae6e
EnginKosure <engin.kosure@cronos.be>
2021-07-07 08:30:32 +0200

    daily py

--- fzlt189.py
+++ fzlt189.py
@@ -1,3 +1,6 @@
+import math
+
+
 def close_compare(a, b, margin=0):
     if margin >= abs(a - b) or a == b:
         return 0
@@ -8,3 +11,7 @@ def close_compare(a, b, margin=0):
 
 def close_compare1(a, b, margin=0):
     return 0 if abs(a - b) <= margin else -1 if b > a else 1
+
+
+def circle_diameter(sides, side_length):
+    return side_length / math.tan(math.pi / sides)
