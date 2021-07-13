9a969b5a2e3bcc0c3d42881e8e9fc988a41353d5
EnginKosure <engin.kosure@cronos.be>
2021-07-13 17:22:33 +0200

    cut docs

--- fzlt192.py
+++ fzlt192.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df = pd.DataFrame({'a': [1, 3, 7, 11, 14, 17]})
 
@@ -7,3 +8,9 @@ bins = [0, 5, 10, 15, 20]
 df['binned'] = pd.cut(df['a'], bins=bins)
 
 print(df)
+
+
+print(pd.cut(np.array([1, 7, 5, 4, 6, 3]),
+             3, labels=["bad", "medium", "good"]))
+# [bad, good, medium, medium, good, bad]
+# Categories (3, object): [bad < medium < good]
