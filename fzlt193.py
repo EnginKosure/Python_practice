d39c747c3ab06a4ed2f25a128f397d0bce9c400f
EnginKosure <engin.kosure@cronos.be>
2021-07-14 21:59:39 +0200

    airflow recap

--- fzlt193.py
+++ fzlt193.py
@@ -8,3 +8,4 @@ dag = DAG(dag_id="etl_update", default_args=default_args)
 # on command line:
 # airflow list_dags # to show all recognized DAGs.
 # airflow -h # for descriptions
+# airflow webserver -p 9090 #To start airflow webserver on port 9090
