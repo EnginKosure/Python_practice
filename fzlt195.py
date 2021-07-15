d5649f8702e152381454bed36c7aa5d2a0918e2f
EnginKosure <engin.kosure@cronos.be>
2021-07-15 10:32:59 +0200

    airflow recap

--- fzlt195.py
+++ fzlt195.py
@@ -1,3 +1,4 @@
+from airflow.operators.email_operator import EmailOperator
 from airflow.operators.python_operator import PythonOperator
 
 
@@ -19,3 +20,19 @@ pull_file_task = PythonOperator(
                'savepath': 'latestsales.json'},
     dag=process_sales_dag
 )
+
+
+# Import the Operator
+
+# Define the task
+email_manager_task = EmailOperator(
+    task_id='email_manager',
+    to='manager@datacamp.com',
+    subject='Latest sales JSON',
+    html_content='Attached is the latest sales JSON file as requested.',
+    files='parsedfile.json',
+    dag=process_sales_dag
+)
+
+# Set the order of tasks
+pull_file_task >> parse_file_task >> email_manager_task
