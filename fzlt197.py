7a5097aabc6a1ef4d67ebfe7603a972f87b0e741
EnginKosure <engin.kosure@cronos.be>
2021-07-17 23:19:23 +0200

    airflow recap

--- fzlt197.py
+++ fzlt197.py
@@ -10,3 +10,16 @@ default_args = {
 # Add to the DAG
 test_dag = DAG('test_workflow', default_args=default_args,
                schedule_interval='@None')
+
+
+test_dag = DAG('test_workflow', start_date=datetime(
+    2020, 2, 20), schedule_interval='@None')
+
+
+# After completing the SLA on the entire workflow, you realize you really
+# only need the SLA timing on a specific task instead of the full workflow.
+# Create the task with the SLA
+task1 = BashOperator(task_id='first_task',
+                     sla=timedelta(hours=3),
+                     bash_command='initialize_data.sh',
+                     dag=test_dag)
