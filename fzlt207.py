c9eefb4250674a7fcc8ead9ecf9b807eea495720
EnginKosure <engin.kosure@cronos.be>
2021-07-20 23:21:28 +0200

    lambda recap

--- fzlt207.py
+++ fzlt207.py
@@ -4,3 +4,8 @@ def add_one(x): return x+1
 
 added = add_one(3)
 print(added)  # 4
+
+numbers = [1, 2, 3, 4]
+times_two = map(lambda x: x * 2, numbers)
+lst = list(times_two)
+print(lst)  # [2, 4, 6, 8]
