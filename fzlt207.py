01bc73fdfb11e3e9e4689c4674b26f9ae06505b1
EnginKosure <engin.kosure@cronos.be>
2021-07-21 11:15:30 +0200

    lambda recap

--- fzlt207.py
+++ fzlt207.py
@@ -9,3 +9,20 @@ numbers = [1, 2, 3, 4]
 times_two = map(lambda x: x * 2, numbers)
 lst = list(times_two)
 print(lst)  # [2, 4, 6, 8]
+
+
+def dispatch_dict(operator, x, y):
+    return {
+        'add': lambda: x + y,
+        'sub': lambda: x - y,
+        'mul': lambda: x * y,
+        'div': lambda: x / y,
+    }.get(operator, lambda: None)()
+
+
+dispatch_dict('add', 2, 8)
+# 10
+dispatch_dict('mul', 2, 8)
+print(dispatch_dict('mul', 2, 8))  # 16
+dispatch_dict('unknown', 2, 8)
+print(dispatch_dict('unknown', 2, 8))  # None
