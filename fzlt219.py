2a7d540ab38791dda6539ab1fee9528dc9a46d8d
EnginKosure <engin.kosure@cronos.be>
2021-07-31 21:39:10 +0200

    sqlite 3 recap

--- fzlt219.py
+++ fzlt219.py
@@ -8,12 +8,18 @@ cursor = connection.cursor()
 # CREATE TABLE IF NOT EXISTS Movies(Title TEXT, Director TEXT, Year INT)
 # ''')
 
-cursor.execute(
-    "INSERT INTO Movies VALUES ('Taxi Driver', 'Martin Scorsese', 1995)")
+
+famous_films = [('Pulp Fiction', 'Quentin Tarantino', 1994),
+                ('Back to the Future', 'Steven Spielberg', 1985),
+                ('Moonrise Kingdom', 'Wes Anderson', 2012)]
+# cursor.execute(
+#     "INSERT INTO Movies VALUES ('Taxi Driver', 'Martin Scorsese', 1995)")
+
+cursor.executemany('Insert INTO Movies VALUES (?,?,?)', famous_films)
 
 cursor.execute("SELECT * FROM Movies")
 
-print(cursor.fetchone())
+print(cursor.fetchall())
 
 connection.commit()
 connection.close()
