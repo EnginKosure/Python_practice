a77fe16b66d59a674424334a3a8f22ecae10298f
EnginKosure <engin.kosure@cronos.be>
2021-07-31 21:23:14 +0200

    sqlite 3 recap

--- fzlt219.py
+++ fzlt219.py
@@ -4,9 +4,16 @@ connection = sqlite3.connect('movies.db')
 
 cursor = connection.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Movies(Title TEXT, Director TEXT, Year INT)
-''')
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS Movies(Title TEXT, Director TEXT, Year INT)
+# ''')
+
+cursor.execute(
+    "INSERT INTO Movies VALUES ('Taxi Driver', 'Martin Scorsese', 1995)")
+
+cursor.execute("SELECT * FROM Movies")
+
+print(cursor.fetchone())
 
 connection.commit()
 connection.close()
