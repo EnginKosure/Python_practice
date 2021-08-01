076775c217ae00d7d9cf64c93520ced01cdef07a
EnginKosure <engin.kosure@cronos.be>
2021-08-01 15:38:12 +0200

    sqlite 3 recap

--- fzlt219.py
+++ fzlt219.py
@@ -9,20 +9,26 @@ cursor = connection.cursor()
 # ''')
 
 
-famous_films = [('Pulp Fiction', 'Quentin Tarantino', 1994),
-                ('Back to the Future', 'Steven Spielberg', 1985),
-                ('Moonrise Kingdom', 'Wes Anderson', 2012)]
+# famous_films = [('Pulp Fiction', 'Quentin Tarantino', 1994),
+#                 ('Back to the Future', 'Steven Spielberg', 1985),
+#                 ('Moonrise Kingdom', 'Wes Anderson', 2012)]
 # cursor.execute(
 #     "INSERT INTO Movies VALUES ('Taxi Driver', 'Martin Scorsese', 1995)")
 
-cursor.executemany('Insert INTO Movies VALUES (?,?,?)', famous_films)
+# cursor.executemany('Insert INTO Movies VALUES (?,?,?)', famous_films)
 
-records = cursor.execute("SELECT * FROM Movies")
+# records = cursor.execute("SELECT * FROM Movies")
 
 # print(cursor.fetchall())
 
-for record in records:
-    print(record)
+# for record in records:
+#     print(record)
+
+release_year = (1985,)
+
+cursor.execute("SELECT * FROM Movies WHERE year=?", release_year)
+
+print(cursor.fetchall())
 
 connection.commit()
 connection.close()
