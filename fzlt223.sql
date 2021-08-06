a597d5c0fa7ded985284612b6dcca0fb4e7a1a2a
EnginKosure <engin.kosure@cronos.be>
2021-08-06 21:58:50 +0200

    daily SQL

--- fzlt223.sql
+++ fzlt223.sql
@@ -1,9 +1,9 @@
-SELECT id, account_id, total_amt_usd
-FROM orders
-ORDER BY total_amt_usd DESC
-LIMIT 5;
+-- SELECT id, account_id, total_amt_usd
+-- FROM orders
+-- ORDER BY total_amt_usd DESC
+-- LIMIT 5;
 
 --SELECT num FROM UNNEST(GENERATE_ARRAY(2012,EXTRACT(YEAR FROM CURRENT_DATE())  )) AS num;
 SELECT *
 FROM accounts
-WHERE name NOT LIKE 'C%' AND name NOT LIKE '%s' 
+WHERE name NOT LIKE 'C%' AND name NOT LIKE '%s' ;
