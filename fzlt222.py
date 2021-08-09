19632b88a8cfb594e10fb518b4432caace8a3c06
EnginKosure <engin.kosure@cronos.be>
2021-08-09 14:44:35 +0200

    daily python

--- fzlt222.py
+++ fzlt222.py
@@ -1,6 +1,14 @@
+import re
+
+
 def test_distinct(data):
     return True if len(data) == len(set(data)) else False
 
 
 print(test_distinct([1, 5, 7, 9]))
 print(test_distinct([2, 4, 5, 5, 7, 9]))
+
+
+def to_camel_case(text):
+    words = re.split(r"[-_]", text)
+    return "".join([words[0]] + [word.title() for word in words[1:]]) if words else ""
