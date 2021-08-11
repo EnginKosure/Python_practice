5b540744bb0a36ba9fa93d661fdb7d69751da043
EnginKosure <engin.kosure@cronos.be>
2021-08-11 23:19:10 +0200

    daily python

--- fzlt225.py
+++ fzlt225.py
@@ -7,3 +7,10 @@ def name_shuffler1(s):
 
 
 # name_shuffler2 = lambda s: ' '.join(reversed(s.split(' ')))
+
+
+def all_continents(lst):
+    cont = []
+    for i in lst:
+        cont.append(i['continent'])
+    return True if sorted(set(cont)) == ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'] else False
