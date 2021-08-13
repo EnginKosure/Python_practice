6b52f2af77045e1e9f56167ee9817fd29fd7f29b
EnginKosure <engin.kosure@cronos.be>
2021-08-13 22:22:54 +0200

    daily python

---
+++ fzlt226.py
@@ -0,0 +1,28 @@
+lyrics = '''
+On the First day of Christmas
+My true love sent to me
+A partridge in a pear tree.
+
+On the Second day of Christmas
+My true love sent to me
+Two turtle doves, and
+A partridge in a pear tree.
+
+...
+
+On the Twelfth day of Christmas
+My true love sent to me
+Twelve drummers drumming,
+Eleven pipers piping,
+Ten lords a-leaping,
+Nine ladies dancing,
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five gold rings,
+Four calling birds,
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.
+'''
+print(len(lyrics))
