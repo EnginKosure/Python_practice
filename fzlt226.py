d62f343cbd818ef91b8bdaaea20ed91e6a84c70d
EnginKosure <engin.kosure@cronos.be>
2021-08-14 22:52:44 +0200

    daily python

--- fzlt226.py
+++ fzlt226.py
@@ -1,28 +1,9 @@
-lyrics = '''
-On the First day of Christmas
-My true love sent to me
-A partridge in a pear tree.
+def f():
+    song = ""
+    for k in range(12):
+        song += 'On the %s day of Christmas\nMy true love sent to me\n' % 'First^Second^Third^Fourth^Fifth^Sixth^Seventh^Eighth^Ninth^Tenth^Eleventh^Twelfth'.split('^')[k]+'\n'.join(
+            'Twelve drummers drumm*Eleven pipers pip*Ten lords a-leap*Nine ladies danc*Eight maids a-milk*Seven swans a-swimm*Six geese a-lay*Five gold rings,^Four calling birds,^Three French hens,^Two turtle doves, and^A partridge in a pear tree.^'.replace('*', 'ing,^').split('^')[11-k:])+"\n"
+    return song[:-2]
 
-On the Second day of Christmas
-My true love sent to me
-Two turtle doves, and
-A partridge in a pear tree.
 
-...
-
-On the Twelfth day of Christmas
-My true love sent to me
-Twelve drummers drumming,
-Eleven pipers piping,
-Ten lords a-leaping,
-Nine ladies dancing,
-Eight maids a-milking,
-Seven swans a-swimming,
-Six geese a-laying,
-Five gold rings,
-Four calling birds,
-Three French hens,
-Two turtle doves, and
-A partridge in a pear tree.
-'''
-print(len(lyrics))
+print(f())
