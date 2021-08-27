67080a1d3611fdc913131ab87d4706d6c8781258
EnginKosure <engin.kosure@cronos.be>
2021-08-27 22:59:09 +0200

    codewar challenge

--- fzlt231.py
+++ fzlt231.py
@@ -4,7 +4,9 @@ x = "01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17"
 
 
 def stat(x):
-    print([i.split('|') for i in x.split()])
+    for t in range(3):
+        print([int(k) for k in (j[t]
+                                for j in (i.split('|') for i in x.split(',')))])
 
 
 stat(x)
