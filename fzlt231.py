80a96f0bdddaabb82dc54a14e8783e809af0b2a5
EnginKosure <engin.kosure@cronos.be>
2021-08-27 23:21:28 +0200

    daily python

--- fzlt231.py
+++ fzlt231.py
@@ -4,13 +4,19 @@ x = "01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17"
 
 
 def stat(x):
+    av = []
+    ra = []
+    me = []
     for t in range(3):
         r = sorted([int(k) for k in (j[t]
                                      for j in (i.split('|') for i in x.split(',')))])
         rng = max(r)-min(r)
-        avg = sum(r)/len(r)
-        med = r[len(r)//2] if len(r) % 2 else (r[len(r)//2]+r[len(r)//2+1])/2
-        print(rng, avg, med)
+        avg = round(sum(r)/len(r))
+        med = r[len(r)//2] if len(r) % 2 else round((r[len(r)//2]+r[len(r)//2+1])/2)
+        av.append(avg)
+        ra.append(rng)
+        me.append(med)
+    print(ra, av, me)
 
 
 stat(x)
