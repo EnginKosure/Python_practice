ed26698384982c740e82633495b0848c22b2a523
EnginKosure <engin.kosure@cronos.be>
2021-08-30 22:36:57 +0200

    dict recap

--- fzlt232.py
+++ fzlt232.py
@@ -1,5 +1,8 @@
 d = {0: 'np1', 1: 'np2', 2: 'np3', 3: 'np4', 4: 'np5'}
 
-
+s = {'np1',  'np2',  'np3',  'np4', 'np5'}
 for i in range(len(d)):
-    print(d[i])
+    print(d[i],)
+
+for i in s:
+    print(i)  # the sequence is different
