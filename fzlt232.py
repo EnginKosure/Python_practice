ec8846c7b0c690d27589987a4886c9969afcfa5e
EnginKosure <engin.kosure@cronos.be>
2021-08-31 22:53:44 +0200

    del

--- fzlt232.py
+++ fzlt232.py
@@ -1,8 +1,10 @@
 d = {0: 'np1', 1: 'np2', 2: 'np3', 3: 'np4', 4: 'np5'}
 
-s = sorted({'np1',  'np2',  'np3',  'np4', 'np5'}
+s = {'np1',  'np2',  'np3',  'np4', 'np5'}
 for i in range(len(d)):
     print(d[i])
 
-for i in s:
-    print(i)  # the sequence is different
+
+a = [1, 2, 3, 4, 5]
+del a[1::2]
+print(a)
