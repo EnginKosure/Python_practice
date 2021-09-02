dbf31d84e2ee826cebd54cceaca975b440f6365e
EnginKosure <engin.kosure@cronos.be>
2021-09-02 04:37:17 +0200

    daily py

--- fzlt232.py
+++ fzlt232.py
@@ -8,3 +8,7 @@ for i in range(len(d)):
 a = [1, 2, 3, 4, 5]
 del a[1::2]
 print(a)
+
+
+def apple(x):
+    return ("Help yourself to a honeycomb Yorkie for the glovebox.", "It's hotter than the sun!!")[int(x)**2 > 1000]
