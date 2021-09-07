9ddc1b6d7b7fd7f6ef2d2d588c0f0bee15e90312
EnginKosure <engin.kosure@cronos.be>
2021-09-07 02:53:49 +0200

    test

--- fzlt235.py
+++ fzlt235.py
@@ -8,3 +8,11 @@
 # [68, -1, 1, -7, 10, 10] => [-1, 10]
 
 # [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+def multiple_of_index(arr):
+    return [e for i, e in enumerate(arr) if (i != 0) and (e % i == 0)]
+
+
+print(multiple_of_index([68, 1, -7, 10, 10, 10]))
+print(multiple_of_index([22, -6, 32, 82, 9, 25]))
+print(multiple_of_index([-56, -85, 72, -26, -14, 76, -
+                         27, 72, 35, -21, -67, 87, 0, 21, 59, 27, -92, 68]))
