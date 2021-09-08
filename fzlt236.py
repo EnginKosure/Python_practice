c4389adf529e1fcae8194fc37a2d8ce96837315e
EnginKosure <engin.kosure@cronos.be>
2021-09-08 07:26:43 +0200

    daily py

--- fzlt236.py
+++ fzlt236.py
@@ -8,4 +8,8 @@
 # the numerator and denominator of the probability written as a/b
 # (with the smallest possible b) that you will win the game with your three attempts
 def magicdice(n):
-    return (0, 1)
+    return ((2**n)**3-(2**n-1)**3, (2**n)**3)
+    # return (0, 1)
+
+
+print(magicdice(5))  # (2977, 32768)
