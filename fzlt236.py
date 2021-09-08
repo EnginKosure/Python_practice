4d39797019e64ccf61b87649fda6134ca76c2547
EnginKosure <engin.kosure@cronos.be>
2021-09-08 07:19:29 +0200

    daily py

---
+++ fzlt236.py
@@ -0,0 +1,11 @@
+# Each player rolls his dice three times.
+# Each player takes the maximal value of his three results.
+# The player with the larger number wins.
+
+# INPUT: an integer number n>=0
+
+# OUTPUT: two integers a and b,
+# the numerator and denominator of the probability written as a/b
+# (with the smallest possible b) that you will win the game with your three attempts
+def magicdice(n):
+    return (0, 1)
