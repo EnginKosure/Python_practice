022f3abe4c3cfbbfa473107c2511b8417400799d
EnginKosure <engin.kosure@cronos.be>
2021-09-11 22:31:46 +0200

    daily py

--- fzlt238.py
+++ fzlt238.py
@@ -5,3 +5,13 @@ def boolean_to_string(b):
 
 
 print(boolean_to_string(True))
+
+
+def high_and_low(s):
+    # print(f"{sorted(s.split(' '))[-1]} {sorted(s.split(' '))[0]}")
+    return f"{sorted(s.split(' '))[-1]} {sorted(s.split(' '))[0]}"
+
+
+high_and_low("1 2 3 4 5")  # return "5 1"
+# high_and_low("1 2 -3 4 5")  # return "5 -3"
+# high_and_low("1 9 3 4 -5")  # return "9 -5"
