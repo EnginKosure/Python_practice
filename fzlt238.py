0b5313ce534d53f5df270e10cc6962d97c74c213
EnginKosure <engin.kosure@cronos.be>
2021-09-11 22:39:58 +0200

    daily py

--- fzlt238.py
+++ fzlt238.py
@@ -8,10 +8,11 @@ print(boolean_to_string(True))
 
 
 def high_and_low(s):
-    # print(f"{sorted(s.split(' '))[-1]} {sorted(s.split(' '))[0]}")
-    return f"{sorted(s.split(' '))[-1]} {sorted(s.split(' '))[0]}"
+    # print(sorted(s.split(' ')))
+    # print(sorted(int(i) for i in s.split(' ')))
+    return f"{sorted(int(i) for i in s.split(' '))[-1]} {sorted(int(i) for i in s.split(' '))[0]}"
 
 
-high_and_low("1 2 3 4 5")  # return "5 1"
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))  # return "5 1"
 # high_and_low("1 2 -3 4 5")  # return "5 -3"
 # high_and_low("1 9 3 4 -5")  # return "9 -5"
