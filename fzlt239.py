92a9e40a5feb2400e2b489b77505ce06dbfb65cc
EnginKosure <engin.kosure@cronos.be>
2021-09-13 23:23:48 +0200

    daily py

--- fzlt239.py
+++ fzlt239.py
@@ -6,7 +6,12 @@ def fix_the_meerkat(arr):
 # N = 12
 # 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) + (1 + 1) + (1 + 2) = 51
 def compute_sum(n):
-    return sum([i if i < 10 else (i//10+i % 10) for i in range(n+1)])
+    s = 0
+    for i in range(n+1):
+        while i:
+            i, remainder = divmod(i, 10)
+            s += remainder
+    return s
 
 
 print(compute_sum(12))
