159dd33e2e56003a4cfce411d87d675ada37f184
EnginKosure <engin.kosure@cronos.be>
2021-09-17 23:57:22 +0200

    refactor

--- fzlt241.py
+++ fzlt241.py
@@ -2,3 +2,7 @@ def contain_all_rots(strng, arr):
     if strng == '':
         return True
     return [sorted(i) for i in arr].count(sorted(strng)) >= len(strng)
+
+
+def contain_all_rots1(s, l):
+    return all(s[i:]+s[:i] in l for i in range(len(s)))
