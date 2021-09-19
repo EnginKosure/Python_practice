1a8ea4777f1810ca8c3c4c945dd62a0b5d493568
EnginKosure <engin.kosure@cronos.be>
2021-09-19 08:51:10 +0200

    filter, lambda recap

--- fzlt241.py
+++ fzlt241.py
@@ -6,3 +6,7 @@ def contain_all_rots(strng, arr):
 
 def contain_all_rots1(s, l):
     return all(s[i:]+s[:i] in l for i in range(len(s)))
+
+
+def even_numbers(arr, n):
+    return list(filter(lambda x: not x % 2, arr))[-n:]
