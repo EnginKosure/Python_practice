e871fc4a063f830d708478a7baabcf22eb5c5861
EnginKosure <engin.kosure@cronos.be>
2021-09-24 22:49:25 +0200

    daily py

---
+++ fzlt243.py
@@ -0,0 +1,14 @@
+import json
+from collections import Counter
+
+month = []
+birthdays = {
+    "Albert Einstein": "03/14/1879",
+    "Ada Byron Lovelace": "12/10/1815",
+    "Benjamin Franklin": "01/17/1706"
+}
+
+for x in birthdays.values():
+    month.append(x.split()[0].split('/')[0])
+
+print(Counter(month))
