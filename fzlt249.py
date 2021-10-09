431695be76197c7a23bb9b27f2d6f4cc00cd4be4
EnginKosure <engin.kosure@cronos.be>
2021-10-09 21:57:05 +0200

    daily python

--- fzlt249.py
+++ fzlt249.py
@@ -11,4 +11,9 @@ y = [5, 10, 15]
 customlist = [a*b for a in x for b in y if a*b % 2 != 0]
 
 aTuple = ("Orange", [10, 20, 30], (5, 15, 25))
-print(aTuple[1][1])
+print(aTuple[2][1])
+
+
+tuple1 = (11, [22, 33], 44, 55)
+tuple1[1][0] = 222
+print(tuple1)
