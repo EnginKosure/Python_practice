a4d7ba83e99881b063bfc60e138ee2a020cb35b2
EnginKosure <engin.kosure@cronos.be>
2021-10-14 21:06:27 +0200

    daily py

--- fzlt252.py
+++ fzlt252.py
@@ -1,8 +1,12 @@
 N = int(input("N: "))
 
 x = 1
+# negative step makes reverse order
 for i in range(N, 0, -1):  # starting number, ending number, step
     numberOfSpaces = i - 1
     numberOfStars = N - i + x
     print(" " * numberOfSpaces + "*" * numberOfStars)
     x = x + 1
+
+
+print(list(range(5, 1, -1)))  # [5, 4, 3, 2]
