7ec7d91f0f198632fceff40b423c62e05c8180bf
EnginKosure <engin.kosure@cronos.be>
2021-11-01 23:54:02 +0100

    break, continue recap

---
+++ fzlt258.py
@@ -0,0 +1,29 @@
+# Program to show the use of continue statement inside loops
+
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+
+print("The end")
+# s
+# t
+# r
+# n
+# g
+# The end
+
+
+# Program to show the use of break statement inside loops
+
+for val in "string":
+    if val == "i":
+        break
+    print(val)
+
+print("The end")
+
+# s
+# t
+# r
+# The end
