5a79ccae1ff40d8583cf5ade38266c63f85956ce
EnginKosure <engin.kosure@cronos.be>
2021-11-06 23:23:40 +0100

    daily py

--- fzlt260.py
+++ fzlt260.py
@@ -1,3 +1,12 @@
 Months = ["Jan", "Feb", "Mar", "April", "May", "June"]
 for i, m in enumerate(Months):
     print(i, m)
+
+
+for i in '123':
+    print("python", i,)
+
+
+Dict = {'Tim': 18, 'Charlie': 12, 'Tiffany': 22, 'Robert': 25}
+Dict.update({"Sarah": 9})
+print(Dict)
