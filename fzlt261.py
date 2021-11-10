8d822fd91352a754d1d4181b36411619d598afa3
EnginKosure <engin.kosure@cronos.be>
2021-11-10 22:19:02 +0100

    daily py, 2d array recap

--- fzlt261.py
+++ fzlt261.py
@@ -1,3 +1,4 @@
+import numpy as np
 w, h = 8, 5
 Matrix = [[0 for x in range(w)] for y in range(h)]
 
@@ -8,3 +9,7 @@ Matrix[0][0] = 1
 Matrix[0][1] = 9
 
 print(Matrix)
+
+
+np_arr = np.ones((4, 6))
+print(np_arr)
