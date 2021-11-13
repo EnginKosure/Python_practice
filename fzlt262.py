fa1dad7b73b7364d5c5edae8f9a2fd00cddaf498
EnginKosure <engin.kosure@cronos.be>
2021-11-14 00:21:48 +0100

    daily py

--- fzlt262.py
+++ fzlt262.py
@@ -1,10 +1,16 @@
-def fact_of_fact():
-    pass
+from math import factorial
 
 
-fact_of_fact(4)  # 288
+def fact_of_fact(n):
+    a = 1
+    for i in range(1, n+1):
+        a *= factorial(i)
+    return a
+
+
+print(fact_of_fact(5))  # 288
 # 4! * 3! * 2! * 1! = 288
 
-fact_of_fact(5)  # 34560
+# fact_of_fact(5)  # 34560
 
-fact_of_fact(6)  # 24883200
+# fact_of_fact(6)  # 24883200
