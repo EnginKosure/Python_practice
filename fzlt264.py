e4ad2d2d11e79589df61d474523fdea2e394af7d
EnginKosure <engin.kosure@cronos.be>
2021-11-18 21:11:29 +0100

    daily py

--- fzlt264.py
+++ fzlt264.py
@@ -3,3 +3,11 @@ import sys
 # Check The Memory Usage Of An Object.
 x = 1
 print(sys.getsizeof(x))
+
+test = ['I', 'Like', 'Python', 'automation']
+
+print(' '.join(test))
+
+
+test1 = [1, 2, 3, 4, 2, 2, 3, 1, 4, 4, 4]
+print(max(set(test1), key=test1.count))
