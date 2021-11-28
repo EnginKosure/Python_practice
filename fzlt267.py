a858f9f7f473dd1151391218b35743b8b6c71bf7
EnginKosure <engin.kosure@cronos.be>
2021-11-28 18:39:52 +0100

    add exp.

--- fzlt267.py
+++ fzlt267.py
@@ -17,4 +17,3 @@ def count_hi(p):
 print(count_hi('abc hi ho'))  # 1
 print(count_hi('ABChi hi'))  # 2
 print(count_hi('hihi'))  # 2
-print("\a")
--- fzlt268.py
+++ fzlt268.py
@@ -3,5 +3,5 @@ import time
 for i in range(10):
     for i in range(4):
         time.sleep(0.5)
-        print("\a")
+        print("\a")  # \a is reserved to warning notification sound in Windows
     time.sleep(3)
