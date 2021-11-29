361e974607cf7a993659fb81eb41c3d3953821e3
EnginKosure <engin.kosure@cronos.be>
2021-11-29 22:54:10 +0100

    daily py

--- fzlt268.py
+++ fzlt268.py
@@ -5,3 +5,9 @@ for i in range(10):
         time.sleep(0.5)
         print("\a")  # \a is reserved to warning notification sound in Windows
     time.sleep(3)
+
+
+lst = "10, 50, 100, 500, 1000, 5000"
+for i in lst:
+    if i == str(0):
+        print(i, end="|")
