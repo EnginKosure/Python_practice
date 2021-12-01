746071b98dfdf58cb413518f99aeebb0d8c53d86
EnginKosure <engin.kosure@cronos.be>
2021-12-01 23:23:23 +0100

    daily py

--- fzlt268.py
+++ fzlt268.py
@@ -1,13 +1,19 @@
 import time
 
-for i in range(10):
-    for i in range(4):
-        time.sleep(0.5)
-        print("\a")  # \a is reserved to warning notification sound in Windows
-    time.sleep(3)
+# for i in range(10):
+#     for i in range(4):
+#         time.sleep(0.5)
+#         print("\a")  # \a is reserved to warning notification sound in Windows
+#     time.sleep(3)
 
 
 lst = "10, 50, 100, 500, 1000, 5000"
 for i in lst:
     if i == str(0):
         print(i, end="|")
+
+
+ten_billion = 10_000_000_000
+
+print(f'{ten_billion:,}')
+# 10, 000, 000, 000
