100028960e577e3bd60b1e555c35406b24a172a9
EnginKosure <engin.kosure@cronos.be>
2021-12-01 23:30:18 +0100

    daily py

--- fzlt268.py
+++ fzlt268.py
@@ -16,4 +16,26 @@ for i in lst:
 ten_billion = 10_000_000_000
 
 print(f'{ten_billion:,}')
-# 10, 000, 000, 000
+# 10,000,000,000
+
+
+# from 13 cards
+cards = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+
+# extract ace, 2, 3 only
+# if you won't use the variable in the future, you can assign to _ underscore
+ace, two, three, *_ = cards
+print(ace, two, three)
+# A 2 3
+
+
+# extract ace, 2, 3 only
+ace, two, three, *others = cards
+print(ace, two, three)  # A 2 3
+print(others)  # ['4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+
+# extract ace, [numbers], J, Q, K
+ace, *numbers, J, Q, K = cards
+print(ace, numbers, J, Q, K)
+# A['2', '3', '4', '5', '6', '7', '8', '9', '10'] J Q K
