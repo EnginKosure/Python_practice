e92ebefedf158c00675077473586af272f43eaf8
EnginKosure <engin.kosure@cronos.be>
2021-12-06 23:58:52 +0100

    logging

--- fzlt268.py
+++ fzlt268.py
@@ -75,3 +75,9 @@ for i in range(20):
 
 logging.warning('Watch out!')  # will print a message to the console
 logging.info('I told you so')  # will not print anything
+logging.basicConfig(filename='example.log',
+                    encoding='utf-8', level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
